ee572ba66dde0833d394fcfc795375ec770f0a3b
naminodarie <gengesa@gmail.com>
2019-10-13 17:14:33 +0900

    二分探索を追加

---
+++ AtCoderProject/二分探索.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderProject.Hide
+{
+    // https://bitbucket.org/camypaper/complib/src/master/lib/Algorithms/BinarySearch.cs
+
+
+    static class 二分探索
+    {
+        static int BinarySearch<T>(IList<T> a, T v, IComparer<T> cmp, bool islb)
+        {
+            var l = 0;
+            var r = a.Count - 1;
+            while (l <= r)
+            {
+                var m = (l + r) / 2;
+                var res = cmp.Compare(a[m], v);
+                if (res < 0 || (res == 0 && !islb)) l = m + 1;
+                else r = m - 1;
+            }
+            return l;
+        }
+
+        /// <summary>
+        /// 与えられた比較関数に従って，<paramref name="a"/> の要素のうち，<paramref name="v"/> 以上の要素であるような最小のインデックスを取得します．
+        /// </summary>
+        /// <typeparam name="T"><see cref="IList{T}"/> の要素の型を指定します．</typeparam>
+        /// <param name="a">対象となるコレクション</param>
+        /// <param name="v">対象となる要素</param>
+        /// <param name="f"></param>
+        /// <returns><paramref name="v"/> 以上の要素であるような最小の o-indexed でのインデックス．</returns>
+        /// <remarks> <paramref name="a"/> は比較関数に対して昇順であることを仮定しています．この関数は O(log N) で実行されます．</remarks>
+        public static int LowerBound<T>(this IList<T> a, T v, Comparison<T> f) => BinarySearch(a, v, Comparer<T>.Create(f), true);
+
+        /// <summary>
+        ///　デフォルトの比較関数に従って，<paramref name="a"/> の要素のうち，<paramref name="v"/> 以上の要素であるような最小のインデックスを取得します．
+        /// </summary>
+        /// <typeparam name="T"><see cref="IList{T}"/> の要素の型を指定します．</typeparam>
+        /// <param name="a">対象となるコレクション</param>
+        /// <param name="v">対象となる要素</param>
+        /// <param name="f"></param>
+        /// <returns><paramref name="v"/> 以上の要素であるような最小の o-indexed でのインデックス．</returns>
+        /// <remarks> <paramref name="a"/> は比較関数に対して昇順であることを仮定しています．この関数は O(log N) で実行されます．</remarks>
+        public static int LowerBound<T>(this IList<T> a, T v) => BinarySearch(a, v, Comparer<T>.Default, true);
+
+        /// <summary>
+        /// 与えられた比較関数に従って，<paramref name="a"/> の要素のうち，<paramref name="v"/> より真に大きい要素が現れる最小のインデックスを取得します．
+        /// </summary>
+        /// <typeparam name="T"><see cref="IList{T}"/> の要素の型を指定します．</typeparam>
+        /// <param name="a">対象となるコレクション</param>
+        /// <param name="v">対象となる要素</param>
+        /// <param name="f"></param>
+        /// <returns><paramref name="v"/> 以上の要素であるような最小の o-indexed でのインデックス．</returns>
+        /// <remarks> <paramref name="a"/> は比較関数に対して昇順であることを仮定しています．この関数は O(log N) で実行されます．</remarks>
+        public static int UpperBound<T>(this IList<T> a, T v, Comparison<T> cmp) => BinarySearch(a, v, Comparer<T>.Create(cmp), false);
+
+        /// <summary>
+        ///　デフォルトの比較関数に従って，<paramref name="a"/> の要素のうち，<paramref name="v"/> より真に大きい要素が現れる最小のインデックスを取得します．
+        /// </summary>
+        /// <typeparam name="T"><see cref="IList{T}"/> の要素の型を指定します．</typeparam>
+        /// <param name="a">対象となるコレクション</param>
+        /// <param name="v">対象となる要素</param>
+        /// <param name="f"></param>
+        /// <returns><paramref name="v"/> 以上の要素であるような最小の o-indexed でのインデックス．</returns>
+        /// <remarks> <paramref name="a"/> は比較関数に対して昇順であることを仮定しています．この関数は O(log N) で実行されます．</remarks>
+        public static int UpperBound<T>(this IList<T> a, T v) => BinarySearch(a, v, Comparer<T>.Default, false);
+    }
+}
