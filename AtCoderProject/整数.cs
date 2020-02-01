5a22d33ec1118e746003ad95d7d90a44085645ce
naminodarie <gengesa@gmail.com>
2020-02-01 16:57:14 +0900

    重複を削除

--- AtCoderProject/整数.cs
+++ AtCoderProject/整数.cs
@@ -168,36 +168,5 @@ namespace AtCoderProject.Hide
             }
             return ret;
         }
-        class Comb
-        {
-            private int mod;
-            private long[] fac;
-            private long[] finv;
-            private long[] inv;
-            public Comb(int maxN, int mod)
-            {
-                maxN++;
-                this.mod = mod;
-                this.fac = new long[maxN];
-                this.finv = new long[maxN];
-                this.inv = new long[maxN];
-
-                fac[0] = fac[1] = 1;
-                finv[0] = finv[1] = 1;
-                inv[1] = 1;
-                for (var i = 2; i < maxN; i++)
-                {
-                    fac[i] = fac[i - 1] * i % mod;
-                    inv[i] = mod - inv[mod % i] * (mod / i) % mod;
-                    finv[i] = finv[i - 1] * inv[i] % mod;
-                }
-            }
-            public long Combination(int n, int k)
-            {
-                if (n < k) return 0;
-                if (n < 0 || k < 0) return 0;
-                return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
-            }
-        }
     }
 }
