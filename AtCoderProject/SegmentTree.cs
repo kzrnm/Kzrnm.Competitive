c0bb899d0a3b55acf6d4950d696a752c5bf6d501
naminodarie <gengesa@gmail.com>
2020-02-04 21:52:52 +0900

    Segment Tree,BIT

--- AtCoderProject/SegmentTree.cs
+++ AtCoderProject/SegmentTree.cs
@@ -7,6 +7,54 @@ using System.Threading.Tasks;
 namespace AtCoderProject.Hide
 {
 #pragma warning disable
+    [System.Diagnostics.DebuggerTypeProxy(typeof(BinaryIndexedTreeDebugView))]
+    class BinaryIndexedTree
+    {
+        public void Add(int i, long w)
+        {
+            for (++i; i < tree.Length; i += (i & -i))
+                tree[i] += w;
+        }
+        public long Sum(int toExclusive)
+        {
+            long res = 0;
+            for (var i = toExclusive; i > 0; i -= (i & -i))
+                res += tree[i];
+            return res;
+        }
+        public long Sum(int from, int toExclusive) => Sum(toExclusive) - Sum(from);
+
+        private long[] tree;
+
+        public BinaryIndexedTree(int size)
+        {
+            tree = new long[size + 1];
+        }
+
+        public class BinaryIndexedTreeDebugView
+        {
+            private BinaryIndexedTree bit;
+            public BinaryIndexedTreeDebugView(BinaryIndexedTree bit)
+            {
+                this.bit = bit;
+            }
+
+            [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.RootHidden)]
+            public string[] Tree
+            {
+                get
+                {
+                    var res = new string[bit.tree.Length - 1];
+                    for (var i = 0; i < res.Length; i++)
+                    {
+                        res[i] = $"real:{bit.tree[i + 1]} num:{bit.Sum(i, i + 1)}";
+                    }
+                    return res;
+                }
+            }
+        }
+    }
+
     [System.Diagnostics.DebuggerTypeProxy(typeof(SegmentTreeDebugView))]
     class SegmentTree
     {
@@ -29,7 +77,7 @@ namespace AtCoderProject.Hide
         public SegmentTree(int size)
         {
             origLength = size;
-            for (rootLength = 1; rootLength <= size; rootLength <<= 1) { }
+            for (rootLength = 1; rootLength < size; rootLength <<= 1) { }
             tree = Enumerable.Repeat(defaultValue, 2 * rootLength - 1).ToArray();
         }
 
