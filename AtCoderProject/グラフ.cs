e9cba493082e5a412f8457d7d3d383501c561667
naminodarie <gengesa@gmail.com>
2020-03-19 22:39:14 +0900

    最大フローを追加

--- AtCoderProject/グラフ.cs
+++ AtCoderProject/グラフ.cs
@@ -172,6 +172,59 @@ namespace AtCoderProject.Hide
                     g++;
             return res;
         }
+        public static int MaxFlow(this Node[] graph, int from, int to)
+        {
+            var capacities = new Dictionary<Tuple<int, int>, int>();
+            for (int i = 0; i < graph.Length; i++)
+                foreach (var next in graph[i].children)
+                    capacities.Add(Tuple.Create(i, next), 1);
+
+            var children = new HashSet<int>[graph.Length];
+            for (int i = 0; i < children.Length; i++)
+                children[i] = new HashSet<int>(graph[i].children);
+            var ret = 0;
+            while (true)
+            {
+                var routes = new Tuple<int, int>[children.Length][];
+                var queue = new Queue<int>(children.Length);
+                routes[from] = Array.Empty<Tuple<int, int>>();
+                queue.Enqueue(from);
+                while (queue.Count > 0 && routes[to] == null)
+                {
+                    var cur = queue.Dequeue();
+                    foreach (var child in children[cur])
+                    {
+                        var route = Tuple.Create(cur, child);
+                        if (routes[child] == null && capacities[route] > 0)
+                        {
+                            routes[child] = new Tuple<int, int>[routes[cur].Length + 1];
+                            routes[cur].CopyTo(routes[child], 0);
+                            routes[child][routes[child].Length - 1] = route;
+                            queue.Enqueue(child);
+                        }
+                    }
+                }
+                if (routes[to] == null)
+                    return ret;
+
+                var min = int.MaxValue;
+                foreach (var route in routes[to])
+                    min = Math.Min(min, capacities[route]);
+
+                ret += min;
+                foreach (var route in routes[to])
+                {
+                    capacities[route] -= min;
+
+                    var rev = Tuple.Create(route.Item2, route.Item1);
+                    children[route.Item2].Add(route.Item1);
+                    int v;
+                    capacities.TryGetValue(rev, out v);
+                    capacities[rev] = v + min;
+                }
+            }
+        }
+
     }
 
     class LowestCommonAncestor // 最小共通祖先
@@ -486,6 +539,62 @@ namespace AtCoderProject.Hide.重み付き
         public bool Equals(Node other) => this.index == other.index;
         public override int GetHashCode() => this.index;
         public override string ToString() => $"children: ({string.Join("),(", children)})";
+    }
+
+    static class GraphUtil
+    {
+        public static int MaxFlow(this Node[] graph, int from, int to)
+        {
+            var capacities = new Dictionary<Tuple<int, int>, int>();
+            for (int i = 0; i < graph.Length; i++)
+                foreach (var next in graph[i].children)
+                    capacities.Add(Tuple.Create(i, next.to), next.value);
+
+            var children = new HashSet<int>[graph.Length];
+            for (int i = 0; i < children.Length; i++)
+                children[i] = new HashSet<int>(graph[i].children.Select(c => c.to));
+            var ret = 0;
+            while (true)
+            {
+                var routes = new Tuple<int, int>[children.Length][];
+                var queue = new Queue<int>(children.Length);
+                routes[from] = Array.Empty<Tuple<int, int>>();
+                queue.Enqueue(from);
+                while (queue.Count > 0 && routes[to] == null)
+                {
+                    var cur = queue.Dequeue();
+                    foreach (var child in children[cur])
+                    {
+                        var route = Tuple.Create(cur, child);
+                        if (routes[child] == null && capacities[route] > 0)
+                        {
+                            routes[child] = new Tuple<int, int>[routes[cur].Length + 1];
+                            routes[cur].CopyTo(routes[child], 0);
+                            routes[child][routes[child].Length - 1] = route;
+                            queue.Enqueue(child);
+                        }
+                    }
+                }
+                if (routes[to] == null)
+                    return ret;
+
+                var min = int.MaxValue;
+                foreach (var route in routes[to])
+                    min = Math.Min(min, capacities[route]);
+
+                ret += min;
+                foreach (var route in routes[to])
+                {
+                    capacities[route] -= min;
+
+                    var rev = Tuple.Create(route.Item2, route.Item1);
+                    children[route.Item2].Add(route.Item1);
+                    int v;
+                    capacities.TryGetValue(rev, out v);
+                    capacities[rev] = v + min;
+                }
+            }
+        }
     }
 
     class ShortestPath
