d9b529374508a4efeb70fed0da63e3e95a7664af
naminodarie <gengesa@gmail.com>
2020-04-23 11:55:29 +0900

    using

--- AtCoderProject/Program.cs
+++ AtCoderProject/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using AtCoderProject;
 using ConsoleReader = AtCoderProject.Reader.ConsoleReader;
 using IEnumerable = System.Collections.IEnumerable;
 using IEnumerator = System.Collections.IEnumerator;
