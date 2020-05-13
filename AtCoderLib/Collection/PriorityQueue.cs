4a3a6d3fb077a39a3be8e90ae069badd0645ade4
naminodarie <gengesa@gmail.com>
2020-05-13 22:05:24 +0900

    Key,Value共通の優先度付きキューを作成

--- AtCoderLib/Collection/PriorityQueue.cs
+++ AtCoderLib/Collection/PriorityQueue.cs
@@ -5,21 +5,89 @@ using System.Collections.Generic;
 
 
 [System.Diagnostics.DebuggerDisplay("Count = {" + nameof(Count) + "}")]
-class PriorityQueue<TKey, TValue>
+class PriorityQueue<T>
 {
-    private readonly List<KeyValuePair<TKey, TValue>> data;
-    private readonly IComparer<TKey> comparer;
-    public PriorityQueue() : this(Comparer<TKey>.Default) { }
-    public PriorityQueue(int capacity) : this(capacity, Comparer<TKey>.Default) { }
-    public PriorityQueue(IComparer<TKey> comparer) { this.data = new List<KeyValuePair<TKey, TValue>>(); this.comparer = comparer; }
-    public PriorityQueue(int capacity, IComparer<TKey> comparer) { this.data = new List<KeyValuePair<TKey, TValue>>(capacity); this.comparer = comparer; }
-    public int Count => data.Count;
-    public KeyValuePair<TKey, TValue> Peek => data[0];
-    public void Add(TKey key, TValue value) { data.Add(new KeyValuePair<TKey, TValue>(key, value)); UpdateUp(data.Count - 1); }
-    public KeyValuePair<TKey, TValue> Dequeue() { var res = data[0]; data[0] = data[^1]; data.RemoveAt(data.Count - 1); UpdateDown(0); return res; }
-    private void UpdateUp(int i) { if (i > 0) { var p = (i - 1) >> 1; if (comparer.Compare(data[i].Key, data[p].Key) < 0) { (data[p], data[i]) = (data[i], data[p]); UpdateUp(p); } } }
-    private void UpdateDown(int i) { var n = data.Count; var child = 2 * i + 1; if (child < n) { if (child != n - 1 && comparer.Compare(data[child].Key, data[child + 1].Key) > 0) child++; if (comparer.Compare(data[i].Key, data[child].Key) > 0) { (data[child], data[i]) = (data[i], data[child]); UpdateDown(child); } } }
+    protected readonly List<T> data;
+    protected readonly IComparer<T> comparer;
 
-    [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.RootHidden)] private KeyValuePair<TKey, TValue>[] Items => data.ToArray().Sort((a, b) => comparer.Compare(a.Key, b.Key));
+    public PriorityQueue() : this(Comparer<T>.Default) { }
+    public PriorityQueue(int capacity) : this(capacity, Comparer<T>.Default) { }
+    public PriorityQueue(IComparer<T> comparer)
+    {
+        this.data = new List<T>();
+        this.comparer = comparer;
+    }
+    public PriorityQueue(int capacity, IComparer<T> comparer)
+    {
+        this.data = new List<T>(capacity);
+        this.comparer = comparer;
+    }
+    [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+    public int Count => data.Count;
+
+    public T Peek => data[0];
+
+    public void Add(T value)
+    {
+        data.Add(value);
+        UpdateUp(data.Count - 1);
+    }
+    public T Dequeue()
+    {
+        var res = data[0];
+        data[0] = data[^1];
+        data.RemoveAt(data.Count - 1);
+        UpdateDown(0);
+        return res;
+    }
+    private void UpdateUp(int i)
+    {
+        if (i > 0)
+        {
+            var p = (i - 1) >> 1;
+            if (comparer.Compare(data[i], data[p]) < 0)
+            {
+                (data[p], data[i]) = (data[i], data[p]);
+                UpdateUp(p);
+            }
+        }
+    }
+    private void UpdateDown(int i)
+    {
+        var n = data.Count;
+        var child = 2 * i + 1;
+        if (child < n)
+        {
+            if (child != n - 1 && comparer.Compare(data[child], data[child + 1]) > 0) child++;
+            if (comparer.Compare(data[i], data[child]) > 0)
+            {
+                (data[child], data[i]) = (data[i], data[child]);
+                UpdateDown(child);
+            }
+        }
+    }
+
+    [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.RootHidden)] private T[] Items => data.ToArray().Sort((a, b) => comparer.Compare(a, b));
 }
 
+
+class PriorityQueue<TKey, TValue> : PriorityQueue<KeyValuePair<TKey, TValue>>
+{
+    private class KeyComparer : IComparer<KeyValuePair<TKey, TValue>>
+    {
+        public readonly IComparer<TKey> comparer;
+        public KeyComparer(IComparer<TKey> comparer)
+        {
+            this.comparer = comparer;
+        }
+        public int Compare(KeyValuePair<TKey, TValue> x, KeyValuePair<TKey, TValue> y)
+            => comparer.Compare(x.Key, y.Key);
+    }
+
+    public PriorityQueue() : this(Comparer<TKey>.Default) { }
+    public PriorityQueue(int capacity) : this(capacity, Comparer<TKey>.Default) { }
+    public PriorityQueue(IComparer<TKey> comparer) : base(new KeyComparer(comparer)) { }
+    public PriorityQueue(int capacity, IComparer<TKey> comparer) : base(capacity, new KeyComparer(comparer)) { }
+
+    public void Add(TKey key, TValue value) => Add(new KeyValuePair<TKey, TValue>(key, value));
+}
