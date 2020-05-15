a505f65655aef17ac7eb6b0638a36c9f7388d6e8
naminodarie <gengesa@gmail.com>
2020-05-15 21:15:15 +0900

    座標圧縮ライブラリを作成

---
+++ AtCoderLib/Collection/座標圧縮.cs
@@ -0,0 +1,23 @@
+﻿using AtCoderProject;
+using System.Collections.Generic;
+using System.Linq;
+
+static class 座標圧縮
+{
+    static Dictionary<int, int> Compress(int[] orig)
+    {
+        var ox = new HashSet<int>(orig).ToArray().Sort();
+        var zip = new Dictionary<int, int>();
+        for (int i = 0; i < ox.Length; i++)
+            zip[ox[i]] = i;
+        return zip;
+    }
+    static int[] Compressed(int[] orig)
+    {
+        var res = new int[orig.Length];
+        var zip = Compress(orig);
+        for (int i = 0; i < res.Length; i++)
+            res[i] = zip[orig[i]];
+        return res;
+    }
+}
