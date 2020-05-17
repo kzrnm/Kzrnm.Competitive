4a65323fee5b4c1d9324134a6471a1b3a2c23f3f
naminodarie <gengesa@gmail.com>
2020-05-18 01:42:27 +0900

    座標圧縮を汎用化

--- AtCoderLib/Collection/座標圧縮.cs
+++ AtCoderLib/Collection/座標圧縮.cs
@@ -1,21 +1,24 @@
 ﻿using AtCoderProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 static class 座標圧縮
 {
-    static Dictionary<int, int> Compress(int[] orig)
+    static Dictionary<T, int> Compress<T>(IEnumerable<T> orig)
     {
-        var ox = new HashSet<int>(orig).ToArray().Sort();
-        var zip = new Dictionary<int, int>();
+        var ox = new HashSet<T>(orig).ToArray();
+        if (typeof(IComparable<T>).IsAssignableFrom(typeof(T)))
+            ox.Sort();
+        var zip = new Dictionary<T, int>();
         for (int i = 0; i < ox.Length; i++)
             zip[ox[i]] = i;
         return zip;
     }
-    static int[] Compressed(int[] orig)
+    static int[] Compressed<T>(T[] orig) => Compressed(orig, Compress(orig));
+    static int[] Compressed<T>(T[] orig, Dictionary<T, int> zip)
     {
         var res = new int[orig.Length];
-        var zip = Compress(orig);
         for (int i = 0; i < res.Length; i++)
             res[i] = zip[orig[i]];
         return res;
