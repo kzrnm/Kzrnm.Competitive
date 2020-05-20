5541e85b622842249ebe3d8b8b9661cb61888b96
naminodarie <gengesa@gmail.com>
2020-05-20 14:54:44 +0900

    Permutation

--- AtCoderLib/Collection/順列を求める.cs
+++ AtCoderLib/Collection/順列を求める.cs
@@ -1,30 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 static class 順列を求める
 {
-    static T[][] Permutation<T>(T[] items)
+    static IEnumerable<T[]> Permutation<T>(IEnumerable<T> orig) where T : IComparable<T>
     {
-        static T[][] PermutationImpl(ReadOnlySpan<T> items)
+        var arr = orig.ToArray();
+        while (true)
         {
-            if (items.Length == 0) throw new IndexOutOfRangeException();
-            if (items.Length == 1) return new T[][] { items.ToArray() };
-            var arr = items.ToArray();
-            var size = 1;
-            for (int i = 2; i <= items.Length; i++)
-                size *= i;
-            var ret = new T[size][];
-            for (int i = 0; i < items.Length; i++)
-            {
-                (arr[0], arr[i]) = (arr[i], arr[0]);
-                foreach (var item in PermutationImpl(new ReadOnlySpan<T>(arr).Slice(1)))
-                {
-                    ret[--size] = new T[items.Length];
-                    ret[size][0] = arr[0];
-                    item.CopyTo(ret[size], 1);
-                };
-            }
-            return ret;
+            yield return arr;
+            int i;
+            for (i = arr.Length - 2; i >= 0; i--)
+                if (arr[i].CompareTo(arr[i + 1]) < 0)
+                    break;
+            if (i < 0) break;
+            int j;
+            for (j = arr.Length - 1; j >= 0; j--)
+                if (arr[i].CompareTo(arr[j]) < 0)
+                    break;
+            (arr[i], arr[j]) = (arr[j], arr[i]);
+            arr.AsSpan(i + 1).Reverse();
         }
-        return PermutationImpl(items);
     }
+
 }
