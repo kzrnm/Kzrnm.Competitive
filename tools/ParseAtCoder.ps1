bcc9109146e5992db0bc34457374506cf469a057
naminodarie <gengesa@gmail.com>
2020-06-23 02:22:47 +0900

    Result(bool b)を毎回初期化

--- tools/ParseAtCoder.ps1
+++ tools/ParseAtCoder.ps1
@@ -240,7 +240,7 @@ function Update-Input {
     )
     $mainPath = "$PSScriptRoot\..\AtCoderProject\Main\Program.cs"
     $main = (Get-Content $mainPath -Raw)
-    ($main -replace 'object Calc()[\s\S]*', 'object Calc(){') > $mainPath
+    ($main -replace 'public static string Result\(bool b\)[\s\S]*', ('public static string Result(bool b) => b ? "Yes" : "No";' + "`r`nprivate object Calc(){")) > $mainPath
     $vars | ForEach-Object { $_.ToInit() } >> $mainPath
     "return null;}" >> $mainPath
     $vars | ForEach-Object { $_.ToDefine() } >> $mainPath
