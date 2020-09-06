19498c7933f2ec2f93777380598fd55f31ca5fa3
naminodarie <gengesa@gmail.com>
2020-09-06 11:41:13 +0900

    RollingHashのテスト時間を短くする

--- AtCoderLib.Tests/文字列/RollingHashTests.cs
+++ AtCoderLib.Tests/文字列/RollingHashTests.cs
@@ -5,19 +5,19 @@ namespace AtCoderLib.文字列
 {
     public class RollingHashTests
     {
+        const string aToM = "abcdefghijklm";
         readonly RollingHash rh;
         public RollingHashTests()
         {
-            var aToZ = "abcdefghijklmnopqrstuvwxyz";
-            rh = new RollingHash(aToZ + aToZ);
+            rh = new RollingHash(aToM + aToM);
         }
 
         [Fact]
         public void HashMatch()
         {
-            for (int l = 0; l < 26; l++)
-                for (int r = l + 1; r <= 26; r++)
-                    rh[l..r].Should().Be(rh[(26 + l)..(26 + r)]);
+            for (int l = 0; l < aToM.Length; l++)
+                for (int r = l + 1; r <= aToM.Length; r++)
+                    rh[l..r].Should().Be(rh[(aToM.Length + l)..(aToM.Length + r)]);
         }
         [Fact]
         public void HashNotMatch()
@@ -25,13 +25,13 @@ namespace AtCoderLib.文字列
             var all = 0;
             var dCnt = 0;
             var hashNotMatchCnt = 0;
-            for (int l1 = 0; l1 < 52; l1++)
-                for (int r1 = l1 + 1; r1 <= 52; r1++)
-                    for (int l2 = 0; l2 < 52; l2++)
-                        for (int r2 = l2 + 1; r2 <= 52; r2++)
+            for (int l1 = 0; l1 < (aToM.Length * 2); l1++)
+                for (int r1 = l1 + 1; r1 <= (aToM.Length * 2); r1++)
+                    for (int l2 = 0; l2 < (aToM.Length * 2); l2++)
+                        for (int r2 = l2 + 1; r2 <= (aToM.Length * 2); r2++)
                         {
                             ++all;
-                            if (!(l1 == l2 && r1 == r2) && !(l1 + 26 == l2 && r1 + 26 == r2))
+                            if (!(l1 == l2 && r1 == r2) && !(l1 + aToM.Length == l2 && r1 + aToM.Length == r2))
                             {
                                 ++dCnt;
                                 if (rh[l1..r1] != rh[l2..r2])
@@ -39,8 +39,8 @@ namespace AtCoderLib.文字列
                             }
                             else rh[l1..r1].Should().Be(rh[l2..r2]);
                         }
-            all.Should().Be(53 * 52 / 2 * 53 * 52 / 2);
-            dCnt.Should().Be(all - 53 * 52 / 2 - 27 * 26 / 2);
+            all.Should().Be((aToM.Length * 2 + 1) * (aToM.Length * 2) / 2 * (aToM.Length * 2 + 1) * (aToM.Length * 2) / 2);
+            dCnt.Should().Be(all - (aToM.Length * 2 + 1) * (aToM.Length * 2) / 2 - (aToM.Length + 1) * aToM.Length / 2);
             ((double)hashNotMatchCnt / dCnt).Should().BeGreaterThan(0.99);
         }
     }
