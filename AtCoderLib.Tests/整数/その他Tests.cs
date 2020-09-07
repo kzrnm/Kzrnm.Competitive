3f3c4aa0e2339e924b5541d3511a9ce61a6d1bc2
naminodarie <gengesa@gmail.com>
2020-09-08 01:18:22 +0900

    FloorSum

---
+++ AtCoderLib.Tests/整数/その他Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AtCoderLib.整数
+{
+    public class その他Tests
+    {
+        [Theory]
+        [InlineData(2, 11, 3, 6, 0)]
+        [InlineData(3, 11, 3, 6, 1)]
+        [InlineData(4, 11, 3, 6, 2)]
+        [InlineData(9, 11, 3, 6, 10)]
+        [InlineData(10, 11, 3, 6, 13)]
+        [InlineData(11, 11, 3, 6, 16)]
+        public void FloorSumTest(long n, long m, long a, long b, long expected)
+        {
+            その他.FloorSum(n, m, a, b).Should().Be(expected);
+        }
+    }
+}
