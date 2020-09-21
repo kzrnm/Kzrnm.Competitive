62eaf3e1c52939766ccf40e399fc90a28c7fcfb3
naminodarie <gengesa@gmail.com>
2020-09-21 11:09:52 +0900

    embeder→embedder

--- tools/SourceCodeEmbedder/AclFileInfoWithDependency.cs
+++ tools/SourceCodeEmbedder/AclFileInfoWithDependency.cs
@@ -4,7 +4,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace SourceCodeEmbeder
+namespace SourceCodeEmbedder
 {
     public class AclFileInfoWithDependency
     {
--- tools/SourceCodeEmbedder/README.md
+++ tools/SourceCodeEmbedder/README.md
@@ -1,3 +1,3 @@
-# SourceCodeEmbeder
+# SourceCodeEmbedder
 
 GitHub Actions で `AtCoderLibrary` にソースコードを埋め込むためのライブラリ。
