2525aa4a3a418112a49e60dce901889b24b3672f
naminodarie <gengesa@gmail.com>
2020-10-24 22:51:18 +0900

    累積和をジェネリックに

--- AtCoderLibrary/DataStructure/Sums.cs
+++ AtCoderLibrary/DataStructure/Sums.cs
@@ -1,20 +1,39 @@
-namespace AtCoder
+﻿namespace AtCoder
 {
+
+    /// <summary>
+    /// <see cref="long"/> の累積和を求めます。
+    /// </summary>
+    public class LongSums : Sums<long, LongOperator> { public LongSums(long[] arr) : base(arr) { } }
+
+    /// <summary>
+    /// <see cref="int"/> の累積和を求めます。
+    /// </summary>
+    public class IntSums : Sums<int, IntOperator> { public IntSums(int[] arr) : base(arr) { } }
+
+    /// <summary>
+    /// <see cref="StaticModInt{T}"/> の累積和を求めます。
+    /// </summary>
+    public class StaticModIntSums<T> : Sums<StaticModInt<T>, StaticModIntOperator<T>> where T : struct, IStaticMod { public StaticModIntSums(StaticModInt<T>[] arr) : base(arr) { } }
+
     /// <summary>
     /// 累積和を求めます。
     /// </summary>
-    public class Sums
+    public class Sums<TValue, TOp>
+        where TValue : struct
+        where TOp : struct, IArithmeticOperator<TValue>
     {
-        readonly long[] impl;
+        static readonly TOp op;
+        private readonly TValue[] impl;
         public int Length => impl.Length - 1;
-        public Sums(int[] arr)
+        public Sums(TValue[] arr)
         {
-            impl = new long[arr.Length + 1];
+            impl = new TValue[arr.Length + 1];
             for (var i = 0; i < arr.Length; i++)
-                impl[i + 1] = impl[i] + arr[i];
+                impl[i + 1] = op.Add(impl[i], arr[i]);
         }
-        public long Slice(int from, int length) => impl[from + length] - impl[from];
-        public long this[int toExclusive] => impl[toExclusive];
-        public long this[int from, int toExclusive] => impl[toExclusive] - impl[from];
+        public TValue Slice(int from, int length) => op.Subtract(impl[from + length], impl[from]);
+        public TValue this[int toExclusive] => impl[toExclusive];
+        public TValue this[int from, int toExclusive] => op.Subtract(impl[toExclusive], impl[from]);
     }
 }
