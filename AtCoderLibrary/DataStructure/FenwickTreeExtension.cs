a36dcef1c0f32a65e5e8dc70597d46be21d6b5ad
naminodarie <gengesa@gmail.com>
2020-11-07 22:11:48 +0900

    FenwickTreeの二分探索

--- AtCoderLibrary/DataStructure/FenwickTreeExtension.cs
+++ AtCoderLibrary/DataStructure/FenwickTreeExtension.cs
@@ -1,23 +1,56 @@
-using static AtCoder.Global;
+﻿using static AtCoder.Global;
 
 namespace AtCoder
 {
     public static class FenwickTreeExtension
     {
-        public static int LowerBound<TValue, TOp>(this FenwickTree<TValue, TOp> fw, TValue w)
+        /// <summary>
+        /// <paramref name="fw"/>.Sum(0, i) の値が <paramref name="v"/> 以上となる最小のインデックス i を取得します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: <paramref name="fw"/> の値が非負。</para>
+        /// <para>計算量: O(log <paramref name="n"/>)</para>
+        /// </remarks>
+        public static int LowerBound<TValue, TOp>(this FenwickTree<TValue, TOp> fw, TValue v)
             where TValue : struct
             where TOp : struct, IArithmeticOperator<TValue>, ICompareOperator<TValue>
         {
             var op = default(TOp);
-            if (op.LessThanOrEqual(w, default)) return 0;
+            if (op.LessThanOrEqual(v, default)) return 0;
             int x = 0;
             for (int k = 1 << MSB(fw.data.Length - 1); k > 0; k >>= 1)
             {
                 var nx = x + k;
-                if (nx < fw.data.Length && op.LessThan(fw.data[nx], w))
+                if (nx < fw.data.Length && op.LessThan(fw.data[nx], v))
                 {
                     x = nx;
-                    w = op.Subtract(w, fw.data[nx]);
+                    v = op.Subtract(v, fw.data[nx]);
+                }
+            }
+            return x;
+        }
+
+        /// <summary>
+        /// <paramref name="fw"/>.Sum(0, i) の値が <paramref name="v"/> より大きくなる最小のインデックス i を取得します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: <paramref name="fw"/> の値が非負。</para>
+        /// <para>計算量: O(log <paramref name="n"/>)</para>
+        /// </remarks>
+        public static int UpperBound<TValue, TOp>(this FenwickTree<TValue, TOp> fw, TValue v)
+            where TValue : struct
+            where TOp : struct, IArithmeticOperator<TValue>, ICompareOperator<TValue>
+        {
+            var op = default(TOp);
+            if (op.LessThanOrEqual(v, default)) return 0;
+            int x = 0;
+            for (int k = 1 << MSB(fw.data.Length - 1); k > 0; k >>= 1)
+            {
+                var nx = x + k;
+                if (nx < fw.data.Length && op.LessThanOrEqual(fw.data[nx], v))
+                {
+                    x = nx;
+                    v = op.Subtract(v, fw.data[nx]);
                 }
             }
             return x;
