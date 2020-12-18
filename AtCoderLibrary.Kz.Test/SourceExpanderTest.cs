9558db4f6ba1a7b7617665ee372bc222908ac854
naminodarie <gengesa@gmail.com>
2020-12-19 02:44:12 +0900

    Test Embedder

---
+++ AtCoderLibrary.Kz.Test/SourceExpanderTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using SourceExpander;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AtCoder.Embedding
+{
+    public class SourceExpanderTest
+    {
+        [Fact]
+        public async Task LanguageVersion()
+        {
+            var embedded = await EmbeddedData.LoadFromAssembly(typeof(Global));
+            embedded.AssemblyMetadatas
+                .Should().ContainKey("SourceExpander.EmbeddedLanguageVersion")
+                .WhichValue.Should().Be("8.0");
+        }
+
+        [Fact]
+        public async Task EmbeddedSource()
+        {
+            var embedded = await EmbeddedData.LoadFromAssembly(typeof(Global));
+            embedded.AssemblyMetadatas.Should().ContainKey("SourceExpander.EmbeddedSourceCode.GZipBase32768");
+            embedded.SourceFiles.Select(s => s.FileName)
+                .Should()
+                .HaveCountGreaterThan(2)
+                .And
+                .OnlyContain(name => name.StartsWith("AtCoderLibrary.Kz>"));
+            embedded.SourceFiles.SelectMany(s => s.TypeNames)
+                .Should()
+                .Contain(
+                    "AtCoder.Global",
+                    "AtCoder.FenwickTreeExtension");
+        }
+    }
+}
