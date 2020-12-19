876f3391eb364008abf28b52131033011547fb7a
naminodarie <gengesa@gmail.com>
2020-12-20 00:55:57 +0900

    MyLinq

--- AtCoderLibrary/Math/StaticModIntExtension.cs
+++ AtCoderLibrary/Math/StaticModIntExtension.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using AtCoder;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-namespace AtCoder
+namespace System.Linq
 {
     public static class StaticModIntExtension
     {
