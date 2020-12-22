2d8715c06451e45e0c8fc99c8e16b69e3197c173
naminodarie <gengesa@gmail.com>
2020-12-22 21:28:11 +0900

    scc

--- AtCoderLibrary/Graph/Utils/強連結成分分解.cs
+++ AtCoderLibrary/Graph/Utils/強連結成分分解.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using AtCoder.Internal;
 
 namespace AtCoder.Graph
 {
@@ -9,64 +8,15 @@ namespace AtCoder.Graph
         /// <summary>
         /// 強連結成分分解する
         /// </summary>
-        /// <returns>各ノードの強連結成分分解されたグループ番号</returns>
-        public static int[] Scc<TEdge>(this INode<TEdge>[] graph)
+        public static List<List<int>> Scc<TEdge>(this INode<TEdge>[] graph)
             where TEdge : IEdge
         {
-            int[] Dfs1()
-            {
-                var cix = new int[graph.Length];
-                var jun = new int[graph.Length];
-                var cur = graph.Length;
-                var idx = new Stack<int>(graph.Length);
-
-                for (int i = 0; i < graph.Length; i++)
-                {
-                    if (cix[i] > graph[i].Children.Length) continue;
-                    idx.Push(i);
-                    while (idx.Count > 0)
-                    {
-                        var index = idx.Pop();
-                        var children = graph[index].Children;
-                        ref var ci = ref cix[index];
-                        if (ci < children.Length)
-                        {
-                            var to = children[ci++].To;
-                            idx.Push(index);
-                            idx.Push(to);
-                        }
-                        else if (ci == children.Length)
-                        {
-                            ci++;
-                            jun[--cur] = index;
-                        }
-                    }
-                }
-                return jun;
-            }
-
-            var jun = Dfs1();
-
-            var res = Global.NewArray(graph.Length, -1);
-            var idx = new Stack<int>(graph.Length);
-            var g = 0;
-            foreach (var i in jun)
-            {
-                if (res[i] >= 0)
-                    continue;
-                idx.Push(i);
-                while (idx.Count > 0)
-                {
-                    int index = idx.Pop();
-                    if (res[index] >= 0)
-                        continue;
-                    res[index] = g;
-                    foreach (var r in graph[index].Roots)
-                        idx.Push(r.To);
-                }
-                ++g;
-            }
-            return res;
+            DebugUtil.Assert(graph[0].IsDirected);
+            var sccGraph = new SCCGraph(graph.Length);
+            for (int i = 0; i < graph.Length; i++)
+                foreach (var e in graph[i].Children)
+                    sccGraph.AddEdge(i, e.To);
+            return sccGraph.SCC();
         }
     }
 }
