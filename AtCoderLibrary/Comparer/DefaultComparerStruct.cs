633b112dd873658441f7119cc906a05f83950f2f
naminodarie <gengesa@gmail.com>
2020-12-23 18:29:40 +0900

    DefaultComparerStruct

---
+++ AtCoderLibrary/Comparer/DefaultComparerStruct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtCoder
+{
+#pragma warning disable CA2231 // Overload operator equals on overriding value type Equals
+    public struct DefaultComparerStruct<T> : IComparer<T> where T : IComparable<T>
+#pragma warning restore CA2231 // Overload operator equals on overriding value type Equals
+    {
+        public static DefaultComparerStruct<T> Default { get; } = default;
+        public int Compare(T x, T y) => x.CompareTo(y);
+        public override bool Equals(object obj) => obj is ReverseComparerStruct<T>;
+        public override int GetHashCode() => GetType().GetHashCode();
+    }
+}
