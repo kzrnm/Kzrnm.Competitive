1871223549a5fb1b3850e6148bb2775034abbce7
naminodarie <gengesa@gmail.com>
2021-01-05 01:12:00 +0900

    逆方向BFS

--- AtCoderLibrary/Graph/Utils/最短経路BFS.cs
+++ AtCoderLibrary/Graph/Utils/最短経路BFS.cs
@@ -5,7 +5,7 @@ namespace AtCoder
     public static class 最短経路BFS
     {
         /// <summary>
-        /// <paramref name="from"/> からの最短経路長をBFSで求める。辺が長さを持たないグラフ用
+        /// <paramref name="from"/> からの最短経路長をBFSで求める。到達判定にも有用
         /// </summary>
         public static int[] ShortestPathBFS<TNode, TEdge>(this IGraph<TNode, TEdge> graph, int from)
             where TNode : INode<TEdge>
@@ -19,6 +19,29 @@ namespace AtCoder
             {
                 var cur = queue.Dequeue();
                 foreach (var e in graph[cur].Children)
+                {
+                    var child = e.To;
+                    if (res[child].UpdateMin(res[cur] + 1))
+                        queue.Enqueue(child);
+                }
+            }
+            return res;
+        }
+        /// <summary>
+        /// <paramref name="from"/> から逆方向の最短経路長をBFSで求める。到達判定にも有用
+        /// </summary>
+        public static int[] ShortestPathBFSReverse<TNode, TEdge>(this IGraph<TNode, TEdge> graph, int from)
+            where TNode : INode<TEdge>
+            where TEdge : IEdge
+        {
+            var res = Global.NewArray(graph.Length, int.MaxValue);
+            var queue = new Queue<int>();
+            queue.Enqueue(from);
+            res[from] = 0;
+            while (queue.Count > 0)
+            {
+                var cur = queue.Dequeue();
+                foreach (var e in graph[cur].Roots)
                 {
                     var child = e.To;
                     if (res[child].UpdateMin(res[cur] + 1))
