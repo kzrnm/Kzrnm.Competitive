462e12a209a54819c9c43001b3e22fc642d30ec3
naminodarie <gengesa@gmail.com>
2021-01-06 19:18:27 +0900

    閉路検出を復活

--- AtCoderLibrary/Graph/Utils/強連結成分分解.cs
+++ AtCoderLibrary/Graph/Utils/強連結成分分解.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AtCoder.Internal;
 
 namespace AtCoder
@@ -6,18 +8,121 @@ namespace AtCoder
     public static class 強連結成分分解
     {
         /// <summary>
-        /// 強連結成分分解する
+        /// 強連結成分ごとに ID を割り振り、各頂点の所属する強連結成分の ID が記録された配列を取得します。
         /// </summary>
+        /// <remarks>
+        /// <para>強連結成分の ID はトポロジカルソートされています。異なる強連結成分の頂点 u, v について、u から v に到達できる時、u の ID は v の ID よりも小さくなります。</para>
+        /// <para>計算量: 追加された辺の本数を m として O(n+m)</para>
+        /// </remarks>
+        public static (int groupNum, int[] ids) SCCIDs<TNode, TEdge>(IGraph<TNode, TEdge> graph)
+            where TNode : INode<TEdge>
+            where TEdge : IEdge
+        {
+            var graphArr = graph.AsArray();
+            var edges = new List<(int from, TEdge edge)>();
+            for (int i = 0; i < graphArr.Length; i++)
+                foreach (var e in graphArr[i].Children)
+                    edges.Add((i, e));
+            // R. Tarjan のアルゴリズム
+            var g = new CSR<TEdge>(graphArr.Length, edges);
+            int nowOrd = 0;
+            int groupNum = 0;
+            var visited = new Stack<int>(graphArr.Length);
+            var low = new int[graphArr.Length];
+            var ord = Enumerable.Repeat(-1, graphArr.Length).ToArray();
+            var ids = new int[graphArr.Length];
+
+            for (int i = 0; i < ord.Length; i++)
+            {
+                if (ord[i] == -1)
+                {
+                    DFS(i);
+                }
+            }
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                // トポロジカル順序にするには逆順にする必要がある。
+                ids[i] = groupNum - 1 - ids[i];
+            }
+
+            return (groupNum, ids);
+
+            void DFS(int v)
+            {
+                low[v] = nowOrd;
+                ord[v] = nowOrd++;
+                visited.Push(v);
+
+                // 頂点 v から伸びる有向辺を探索する。
+                for (int i = g.Start[v]; i < g.Start[v + 1]; i++)
+                {
+                    int to = g.EList[i].To;
+                    if (ord[to] == -1)
+                    {
+                        DFS(to);
+                        low[v] = Math.Min(low[v], low[to]);
+                    }
+                    else
+                    {
+                        low[v] = Math.Min(low[v], ord[to]);
+                    }
+                }
+
+                // v がSCCの根である場合、強連結成分に ID を割り振る。
+                if (low[v] == ord[v])
+                {
+                    while (true)
+                    {
+                        int u = visited.Pop();
+                        ord[u] = graphArr.Length;
+                        ids[u] = groupNum;
+
+                        if (u == v)
+                        {
+                            break;
+                        }
+                    }
+
+                    groupNum++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 強連結成分分解の結果である「頂点のリスト」のリストを取得します。
+        /// </summary>
+        /// <remarks>
+        /// <para>- 全ての頂点がちょうど1つずつ、どれかのリストに含まれます。</para>
+        /// <para>- 内側のリストと強連結成分が一対一に対応します。リスト内での頂点の順序は未定義です。</para>
+        /// <para>- リストはトポロジカルソートされています。異なる強連結成分の頂点 u, v について、u から v に到達できる時、u の属するリストは v の属するリストよりも前です。</para>
+        /// <para>計算量: 追加された辺の本数を m として O(n+m)</para>
+        /// </remarks>
         public static List<List<int>> Scc<TNode, TEdge>(this IGraph<TNode, TEdge> graph)
             where TNode : INode<TEdge>
             where TEdge : IEdge
         {
-            DebugUtil.Assert(graph[0].IsDirected);
-            var sccGraph = new SCCGraph(graph.Length);
-            for (int i = 0; i < graph.Length; i++)
-                foreach (var e in graph[i].Children)
-                    sccGraph.AddEdge(i, e.To);
-            return sccGraph.SCC();
+            var (groupNum, ids) = SCCIDs(graph);
+            var counts = new int[groupNum];
+
+            foreach (var x in ids)
+            {
+                counts[x]++;
+            }
+
+            var groups = new List<List<int>>(groupNum);
+
+            for (int i = 0; i < groupNum; i++)
+            {
+                groups.Add(new List<int>(counts[i]));
+            }
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                groups[ids[i]].Add(i);
+            }
+
+            return groups;
         }
     }
 }
---
+++ AtCoderLibrary/Graph/Utils/閉路検出DFS.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace AtCoder
+{
+    public static class 閉路検出DFS
+    {
+        enum Status { None, Active, Done }
+        /// <summary>
+        /// 閉路があれば返す。なければ(-1, null)
+        /// </summary>
+        public static (int from, TEdge[] edges) GetCycleDFS<TNode, TEdge>(this IGraph<TNode, TEdge> graph)
+            where TNode : INode<TEdge>
+            where TEdge : IEdge
+        {
+            var statuses = new Status[graph.Length];
+            (int from, List<TEdge> edges) DFS(Stack<(int v, int childIdx)> stack)
+            {
+                List<TEdge> list = null;
+                while (stack.Count > 0)
+                {
+                    var (v, ci) = stack.Pop();
+                    var children = graph[v].Children;
+                    if (ci == 0)
+                        statuses[v] = Status.Active;
+                    else if (list != null)
+                    {
+                        list.Add(children[ci - 1]);
+                        if (list[0].To == v)
+                            return (v, list);
+                        continue;
+                    }
+
+                    if (ci < children.Length)
+                    {
+                        var e = children[ci];
+                        var child = e.To;
+                        switch (statuses[child])
+                        {
+                            case Status.None:
+                                stack.Push((v, ci + 1));
+                                stack.Push((child, 0));
+                                break;
+                            case Status.Active:
+                                list = new List<TEdge> { e };
+                                break;
+                        }
+                    }
+                    else
+                        statuses[v] = Status.Done;
+                }
+                return (-1, null);
+            }
+            for (var i = 0; i < graph.Length; i++)
+            {
+                if (statuses[i] == Status.None)
+                {
+                    var stack = new Stack<(int v, int childIdx)>();
+                    stack.Push((i, 0));
+                    var (from, res) = DFS(stack);
+                    if (res != null)
+                    {
+                        res.Reverse();
+                        return (from, res.ToArray());
+                    }
+                }
+            }
+            return (-1, null);
+        }
+    }
+}
