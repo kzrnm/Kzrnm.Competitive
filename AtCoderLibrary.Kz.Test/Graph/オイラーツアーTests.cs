6c51d2d7e807307bc8924ef56a2febfbc6bc3c18
naminodarie <gengesa@gmail.com>
2021-01-14 21:36:27 +0900

    テストを修正

--- AtCoderLibrary.Kz.Test/Graph/オイラーツアーTests.cs
+++ AtCoderLibrary.Kz.Test/Graph/オイラーツアーTests.cs
@@ -19,22 +19,22 @@ namespace AtCoder.Graph
             gb.Add(3, 7);
             var tour = gb.ToTree().EulerianTour();
             tour.Events.Should().Equal(
-                new オイラーツアー<Edge>.Event(-1, new Edge(0), false),
-                new オイラーツアー<Edge>.Event(0, new Edge(1), false),
-                new オイラーツアー<Edge>.Event(1, new Edge(3), false),
-                new オイラーツアー<Edge>.Event(3, new Edge(7), false),
-                new オイラーツアー<Edge>.Event(3, new Edge(7), true),
-                new オイラーツアー<Edge>.Event(1, new Edge(3), true),
-                new オイラーツアー<Edge>.Event(1, new Edge(4), false),
-                new オイラーツアー<Edge>.Event(1, new Edge(4), true),
+                new オイラーツアー<Edge>.Event(-1, new Edge(0), true),
                 new オイラーツアー<Edge>.Event(0, new Edge(1), true),
-                new オイラーツアー<Edge>.Event(0, new Edge(2), false),
-                new オイラーツアー<Edge>.Event(2, new Edge(5), false),
-                new オイラーツアー<Edge>.Event(2, new Edge(5), true),
-                new オイラーツアー<Edge>.Event(2, new Edge(6), false),
-                new オイラーツアー<Edge>.Event(2, new Edge(6), true),
+                new オイラーツアー<Edge>.Event(1, new Edge(3), true),
+                new オイラーツアー<Edge>.Event(3, new Edge(7), true),
+                new オイラーツアー<Edge>.Event(3, new Edge(7), false),
+                new オイラーツアー<Edge>.Event(1, new Edge(3), false),
+                new オイラーツアー<Edge>.Event(1, new Edge(4), true),
+                new オイラーツアー<Edge>.Event(1, new Edge(4), false),
+                new オイラーツアー<Edge>.Event(0, new Edge(1), false),
                 new オイラーツアー<Edge>.Event(0, new Edge(2), true),
-                new オイラーツアー<Edge>.Event(-1, new Edge(0), true));
+                new オイラーツアー<Edge>.Event(2, new Edge(5), true),
+                new オイラーツアー<Edge>.Event(2, new Edge(5), false),
+                new オイラーツアー<Edge>.Event(2, new Edge(6), true),
+                new オイラーツアー<Edge>.Event(2, new Edge(6), false),
+                new オイラーツアー<Edge>.Event(0, new Edge(2), false),
+                new オイラーツアー<Edge>.Event(-1, new Edge(0), false));
             Enumerable.Range(0, 8).Select(i => tour[i]).Should().Equal(
                 (0, 15), (1, 8), (9, 14), (2, 5), (6, 7), (10, 11), (12, 13), (3, 4));
         }
@@ -51,22 +51,22 @@ namespace AtCoder.Graph
             gb.Add(3, 7, 7);
             var tour = gb.ToTree().EulerianTour();
             tour.Events.Should().Equal(
-                new オイラーツアー<WEdge<int>>.Event(-1, new WEdge<int>(0, 0), false),
-                new オイラーツアー<WEdge<int>>.Event(0, new WEdge<int>(1, 1), false),
-                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(3, 3), false),
-                new オイラーツアー<WEdge<int>>.Event(3, new WEdge<int>(7, 7), false),
-                new オイラーツアー<WEdge<int>>.Event(3, new WEdge<int>(7, 7), true),
-                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(3, 3), true),
-                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(4, 4), false),
-                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(4, 4), true),
+                new オイラーツアー<WEdge<int>>.Event(-1, new WEdge<int>(0, 0), true),
                 new オイラーツアー<WEdge<int>>.Event(0, new WEdge<int>(1, 1), true),
-                new オイラーツアー<WEdge<int>>.Event(0, new WEdge<int>(2, 2), false),
-                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(5, 5), false),
-                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(5, 5), true),
-                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(6, 6), false),
-                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(6, 6), true),
+                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(3, 3), true),
+                new オイラーツアー<WEdge<int>>.Event(3, new WEdge<int>(7, 7), true),
+                new オイラーツアー<WEdge<int>>.Event(3, new WEdge<int>(7, 7), false),
+                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(3, 3), false),
+                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(4, 4), true),
+                new オイラーツアー<WEdge<int>>.Event(1, new WEdge<int>(4, 4), false),
+                new オイラーツアー<WEdge<int>>.Event(0, new WEdge<int>(1, 1), false),
                 new オイラーツアー<WEdge<int>>.Event(0, new WEdge<int>(2, 2), true),
-                new オイラーツアー<WEdge<int>>.Event(-1, new WEdge<int>(0, 0), true));
+                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(5, 5), true),
+                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(5, 5), false),
+                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(6, 6), true),
+                new オイラーツアー<WEdge<int>>.Event(2, new WEdge<int>(6, 6), false),
+                new オイラーツアー<WEdge<int>>.Event(0, new WEdge<int>(2, 2), false),
+                new オイラーツアー<WEdge<int>>.Event(-1, new WEdge<int>(0, 0), false));
             Enumerable.Range(0, 8).Select(i => tour[i]).Should().Equal(
                 (0, 15),
                 (1, 8), (9, 14), (2, 5), (6, 7), (10, 11), (12, 13), (3, 4));
