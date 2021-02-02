05abbc467a1c9faf71ff0f7d17b3bf659f4efda0
naminodarie <gengesa@gmail.com>
2021-02-02 19:51:26 +0900

    重み付きUnionFind

---
+++ AtCoderLibrary.Kz.Test/Graph/UnionFind/WeightedUnionFindTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+
+namespace AtCoder.Graph
+{
+    public class WeightedUnionFindTests
+    {
+        [Fact]
+        public void Zero()
+        {
+            var uf = new IntWeightedUnionFind(0);
+            uf.Groups().Should().Equal(Array.Empty<int[]>());
+        }
+
+        [Fact]
+        public void Simple()
+        {
+            var uf = new IntWeightedUnionFind(2);
+            uf.Same(0, 1).Should().BeFalse();
+            uf.Merge(0, 1, 5).Should().BeTrue();
+            uf.Same(0, 1).Should().BeTrue();
+            uf.Size(0).Should().Be(2);
+            uf.WeightDiff(0, 1).Should().Be(5);
+            uf.WeightDiff(1, 0).Should().Be(-5);
+        }
+
+        [Fact]
+        public void Line()
+        {
+            int n = 500000;
+            var uf = new IntWeightedUnionFind(n);
+            for (int i = 0; i < n - 1; i++)
+            {
+                uf.Merge(i, i + 1, 1);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                uf.WeightDiff(0, i).Should().Be(i);
+                uf.WeightDiff(10, i).Should().Be(i - 10);
+            }
+            uf.Size(0).Should().Be(n);
+            uf.Groups().Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void LineReverse()
+        {
+            int n = 500000;
+            var uf = new IntWeightedUnionFind(n);
+            for (int i = n - 2; i >= 0; i--)
+            {
+                uf.Merge(i, i + 1, 1);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                uf.WeightDiff(0, i).Should().Be(i);
+                uf.WeightDiff(10, i).Should().Be(i - 10);
+            }
+            uf.Size(0).Should().Be(n);
+            uf.Groups().Should().HaveCount(1);
+        }
+    }
+}
---
+++ AtCoderLibrary/Graph/UnionFind/WeightedUnionFind.cs
@@ -0,0 +1,153 @@
+﻿using AtCoder.Internal;
+using System.Collections.Generic;
+
+namespace AtCoder
+{
+    public class IntWeightedUnionFind : WeightedUnionFind<int, IntOperator> { public IntWeightedUnionFind(int n) : base(n) { } }
+    public class LongWeightedUnionFind : WeightedUnionFind<long, LongOperator> { public LongWeightedUnionFind(int n) : base(n) { } }
+    public class WeightedUnionFind<T, TOp> where TOp : struct, IAdditionOperator<T>, IUnaryNumOperator<T>
+    {
+        private static TOp op = default;
+        internal readonly int _n;
+        internal readonly int[] _parentOrSize;
+        /// <summary>
+        /// 親との重みの差分
+        /// </summary>
+        internal readonly T[] _weightDiff;
+
+        /// <summary>
+        /// <see cref="WeightedUnionFind"/> クラスの新しいインスタンスを、<paramref name="n"/> 頂点 0 辺のグラフとして初期化します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="n"/>≤10^8</para>
+        /// <para>計算量: O(<paramref name="n"/>)</para>
+        /// </remarks>
+        public WeightedUnionFind(int n)
+        {
+            _n = n;
+            _parentOrSize = new int[n];
+            _weightDiff = new T[n];
+            for (int i = 0; i < _parentOrSize.Length; i++) _parentOrSize[i] = -1;
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> から頂点 <paramref name="b"/> を重み <paramref name="w"/> で結ぶ辺を追加します。辺が追加済みでないか既に追加済みで重みが <paramref name="w"/> なら true, 重みに矛盾があるなら false を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public bool Merge(int a, int b, T w)
+        {
+            Contract.Assert(0 <= a && a < _n);
+            Contract.Assert(0 <= b && b < _n);
+            int x = Leader(a), y = Leader(b);
+            if (x == y)
+                return EqualityComparer<T>.Default.Equals(WeightDiff(a, b), w);
+
+            w = op.Add(w, op.Subtract(Weight(a), Weight(b)));
+            if (-_parentOrSize[x] < -_parentOrSize[y])
+            {
+                (x, y) = (y, x);
+                w = op.Minus(w);
+            }
+            _parentOrSize[x] += _parentOrSize[y];
+            _parentOrSize[y] = x;
+            _weightDiff[y] = w;
+            return true;
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/>, <paramref name="b"/> が連結かどうかを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public bool Same(int a, int b)
+        {
+            Contract.Assert(0 <= a && a < _n);
+            Contract.Assert(0 <= b && b < _n);
+            return Leader(a) == Leader(b);
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の属する連結成分の代表元を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Leader(int a)
+        {
+            if (_parentOrSize[a] < 0) return a;
+            int par = Leader(_parentOrSize[a]);
+            _weightDiff[a] = op.Add(_weightDiff[a], _weightDiff[_parentOrSize[a]]);
+            return _parentOrSize[a] = par;
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の重みを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        T Weight(int a)
+        {
+            Leader(a);
+            return _weightDiff[a];
+        }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> と頂点 <paramref name="b"/> の重みの差分を返します。同じグループにない場合の動作は未定義です。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public T WeightDiff(int a, int b) => op.Subtract(Weight(b), Weight(a));
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の属する連結成分のサイズを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Size(int a)
+        {
+            Contract.Assert(0 <= a && a < _n);
+            return -_parentOrSize[Leader(a)];
+        }
+
+        /// <summary>
+        /// グラフを連結成分に分け、その情報を返します。
+        /// </summary>
+        /// <para>計算量: O(n)</para>
+        /// <returns>「一つの連結成分の頂点番号のリスト」のリスト。</returns>
+        public List<int[]> Groups()
+        {
+            int[] leaderBuf = new int[_n];
+            int[] id = new int[_n];
+            var result = new List<int[]>(_n);
+            for (int i = 0; i < leaderBuf.Length; i++)
+            {
+                leaderBuf[i] = Leader(i);
+                if (i == leaderBuf[i])
+                {
+                    id[i] = result.Count;
+                    result.Add(new int[-_parentOrSize[i]]);
+                }
+            }
+            int[] ind = new int[result.Count];
+            for (int i = 0; i < leaderBuf.Length; i++)
+            {
+                var leaderID = id[leaderBuf[i]];
+                result[leaderID][ind[leaderID]] = i;
+                ind[leaderID]++;
+            }
+            return result;
+        }
+    }
+}
