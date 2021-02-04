8975b7777b30ed2e1f141983643b77db9fc44cfd
naminodarie <gengesa@gmail.com>
2021-02-05 00:42:24 +0900

    SegtreeBeats

---
+++ AtCoderLibrary.Kz.Test/DataStructure/SegtreeBeatsTests.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+using Xunit;
+using static System.Math;
+
+namespace AtCoder.DataStructure
+{
+    public class SegtreeBeatsTests
+    {
+        [Fact]
+        public void Native()
+        {
+            LongSums sums;
+            var seg = new SegtreeBeats<S, long, Op>(new S[10].Fill(new S(0)));
+            seg.Apply(0, 8, 1);
+            sums = new LongSums(new long[] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 });
+            for (int l = 0; l < 10; l++)
+                for (int r = l; r <= 10; r++)
+                    seg[l..r].sum.Should().Be(sums[l..r]);
+
+            seg.Apply(0, 3, 5);
+            sums = new LongSums(new long[] { 5, 5, 5, 1, 1, 1, 1, 1, 0, 0 });
+            for (int l = 0; l < 10; l++)
+                for (int r = l; r <= 10; r++)
+                    seg[l..r].sum.Should().Be(sums[l..r]);
+
+            seg.Apply(2, 5, 2);
+            sums = new LongSums(new long[] { 5, 5, 5, 2, 2, 1, 1, 1, 0, 0 });
+            for (int l = 0; l < 10; l++)
+                for (int r = l; r <= 10; r++)
+                    seg[l..r].sum.Should().Be(sums[l..r]);
+        }
+        struct S
+        {
+            public long max;
+            public long max2;
+            public long sum;
+            public int cnt;
+            public int maxCnt;
+            public S(long num, int cnt = 1)
+            {
+                max = num;
+                max2 = long.MinValue >> 2;
+                sum = num * cnt;
+                this.cnt = cnt;
+                maxCnt = cnt;
+            }
+            public override string ToString() => $"max: {max}, max2: {max2}, sum: {sum}, cnt: {cnt}, maxCnt: {maxCnt}";
+        }
+
+        struct Op : ISegtreeBeatsOperator<S, long>
+        {
+            public S Identity => new S
+            {
+                max = long.MinValue >> 2,
+                max2 = long.MinValue >> 2,
+            };
+
+            public long FIdentity => long.MinValue >> 2;
+
+            public S Operate(S x, S y) => new S
+            {
+                max = Max(x.max, y.max),
+                max2 = Max2(x.max, x.max2, y.max, y.max2),
+                sum = x.sum + y.sum,
+                cnt = x.cnt + y.cnt,
+                maxCnt = x.max == y.max ? x.maxCnt + y.maxCnt : x.max > y.max ? x.maxCnt : y.maxCnt,
+            };
+
+            long Max2(long v1, long v2, long v3, long v4)
+            {
+                if (v1 == v3) return Max(v2, v4);
+                if (v1 > v3) return Max(v2, v3);
+                else return Max(v1, v4);
+            }
+            public bool Mapping(long f, S x, out S res)
+            {
+                if (x.cnt == 0)
+                {
+                    res = Identity;
+                    return true;
+                }
+                if (f >= x.max)
+                {
+                    res = new S(f, x.cnt);
+                    return true;
+                }
+                if (x.cnt == x.maxCnt)
+                {
+                    res = x;
+                    return true;
+                }
+                if (f > x.max2)
+                {
+                    res = x;
+                    res.max2 = f;
+                    res.sum = x.max * x.maxCnt + (x.cnt - x.maxCnt) * f;
+                    return true;
+                }
+                res = x;
+                return false;
+            }
+            public long Composition(long nf, long cf) => Max(nf, cf);
+        }
+    }
+}
---
+++ LibraryChecker/Solvers/DataStructure/SegtreeBeatsSolver.cs
@@ -0,0 +1,153 @@
+﻿using Kzrnm.Competitive.IO;
+using System.Linq;
+using static System.Math;
+
+namespace AtCoder.Solvers.DataStructure
+{
+    public class SegtreeBeatsSolver : ISolver
+    {
+        public string Name => "range_chmin_chmax_add_range_sum";
+
+        public void Solve(ConsoleReader cr, ConsoleWriter cw)
+        {
+            int N = cr;
+            int Q = cr;
+            long[] a = cr.Repeat(N);
+            var seg = new SegtreeBeats<S, F, Op>(a.Select(n => new S(n)).ToArray());
+            for (int q = 0; q < Q; q++)
+            {
+                int t = cr;
+                int l = cr;
+                int r = cr;
+                if (t == 3)
+                    cw.WriteLine(seg[l..r].sum);
+                else
+                {
+                    long b = cr;
+                    if (t == 0)
+                        seg.Apply(l, r, F.Min(b));
+                    else if (t == 1)
+                        seg.Apply(l, r, F.Max(b));
+                    else
+                        seg.Apply(l, r, F.Add(b));
+                }
+            }
+        }
+    }
+    struct S
+    {
+        public long min;
+        public long max;
+        public long max2;
+        public long min2;
+        public long sum;
+        public int cnt;
+        public int minCnt;
+        public int maxCnt;
+        public S(long num, int cnt = 1)
+        {
+            min = num;
+            max = num;
+            min2 = long.MaxValue >> 2;
+            max2 = long.MinValue >> 2;
+            sum = num * cnt;
+            this.cnt = cnt;
+            minCnt = cnt;
+            maxCnt = cnt;
+        }
+    }
+    struct F
+    {
+        public long min;
+        public long max;
+        public long sum;
+
+        public static F Min(long num) => new F { min = num, max = long.MinValue >> 2 };
+        public static F Max(long num) => new F { min = long.MaxValue >> 2, max = num };
+        public static F Add(long num) => new F { min = long.MaxValue >> 2, max = long.MinValue >> 2, sum = num };
+    }
+
+    struct Op : ISegtreeBeatsOperator<S, F>
+    {
+        public S Identity => new S
+        {
+            min = long.MaxValue >> 2,
+            max = long.MinValue >> 2,
+            min2 = long.MaxValue >> 2,
+            max2 = long.MinValue >> 2,
+        };
+
+        public F FIdentity => new F
+        {
+            min = long.MaxValue >> 2,
+            max = long.MinValue >> 2,
+        };
+
+        public S Operate(S x, S y) => new S
+        {
+            min = Min(x.min, y.min),
+            max = Max(x.max, y.max),
+            min2 = Min2(x.min, x.min2, y.min, y.min2),
+            max2 = Max2(x.max, x.max2, y.max, y.max2),
+            sum = x.sum + y.sum,
+            cnt = x.cnt + y.cnt,
+            minCnt = x.min == y.min ? x.minCnt + y.minCnt : x.min < y.min ? x.minCnt : y.minCnt,
+            maxCnt = x.max == y.max ? x.maxCnt + y.maxCnt : x.max > y.max ? x.maxCnt : y.maxCnt,
+        };
+
+        long Min2(long v1, long v2, long v3, long v4)
+        {
+            if (v1 == v3) return Min(v2, v4);
+            if (v1 < v3) return Min(v2, v3);
+            else return Min(v1, v4);
+        }
+
+        long Max2(long v1, long v2, long v3, long v4)
+        {
+            if (v1 == v3) return Max(v2, v4);
+            if (v1 > v3) return Max(v2, v3);
+            else return Max(v1, v4);
+        }
+        public bool Mapping(F f, S x, out S res)
+        {
+            if (x.cnt == 0)
+            {
+                res = Identity;
+                return true;
+            }
+            if (x.min == x.max || f.max == f.min || f.max >= x.max || f.min < x.min)
+            {
+                res = new S(Min(f.min, Max(x.min, f.max)) + f.sum, x.cnt);
+                return true;
+            }
+            if (x.min2 == x.max)
+            {
+                x.min = x.max2 = Max(x.min, f.max) + f.sum;
+                x.max = x.min2 = Min(x.max, f.min) + f.sum;
+                x.sum = x.min * x.minCnt + x.max * x.maxCnt;
+                res = x;
+                return true;
+            }
+            if (f.max < x.min2 && f.min > x.max2)
+            {
+                var nxt_lo = Max(x.min, f.max);
+                var nxt_hi = Min(x.max, f.min);
+                x.sum += (nxt_lo - x.min) * x.minCnt - (x.max - nxt_hi) * x.maxCnt + f.sum * x.cnt;
+                x.min = nxt_lo + f.sum;
+                x.max = nxt_hi + f.sum;
+                x.min2 += f.sum;
+                x.max2 += f.sum;
+                res = x;
+                return true;
+            }
+            res = x;
+            return false;
+        }
+        public F Composition(F nf, F cf) => new F
+        {
+            max = Max(nf.max, Min(cf.max + cf.sum, nf.min)) - cf.sum,
+            min = Min(nf.min, Max(cf.min + cf.sum, nf.max)) - cf.sum,
+            sum = cf.sum + nf.sum,
+        };
+    }
+}
