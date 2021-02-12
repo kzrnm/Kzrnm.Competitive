1a87d4e948026cff619aff405ea80e0e6aa7253a
naminodarie <gengesa@gmail.com>
2021-02-13 01:53:25 +0900

    Gridのサイズ

--- Competitive.Library/Util/Grid.cs
+++ Competitive.Library/Util/Grid.cs
@@ -11,13 +11,13 @@ namespace Kzrnm.Competitive
         public static Grid<char> GridString(this PropertyConsoleReader cr, int H, char defaultValue) => Create(cr.Repeat(H).Ascii, defaultValue);
         public static Grid<int> GridInt(this PropertyConsoleReader cr, int H, int W) => Create(cr.Repeat(H).Select(cr => cr.Repeat(W).Int));
         public static Grid<int> GridInt(this PropertyConsoleReader cr, int H, int W, int defaultValue) => Create(cr.Repeat(H).Select(cr => cr.Repeat(W).Int), defaultValue);
-        public static Grid<char> Create(string[] data) => new Grid<char>(CollectionUtil.Flatten(data), data.Length, data[0].Length);
+        public static Grid<char> Create(string[] data) => new Grid<char>(CollectionUtil.Flatten(data), data.Length, data[0].Length, default(char));
         public static Grid<char> Create(string[] data, char defaultValue) => new Grid<char>(CollectionUtil.Flatten(data), data.Length, data[0].Length, defaultValue);
-        public static Grid<T> Create<T>(T[][] data) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length);
+        public static Grid<T> Create<T>(T[][] data) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length, default(T));
         public static Grid<T> Create<T>(T[][] data, T defaultValue) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length, defaultValue);
-        public static Grid<T> Create<T>(Span<T[]> data) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length);
+        public static Grid<T> Create<T>(Span<T[]> data) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length, default(T));
         public static Grid<T> Create<T>(Span<T[]> data, T defaultValue) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length, defaultValue);
-        public static Grid<T> Create<T>(ReadOnlySpan<T[]> data) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length);
+        public static Grid<T> Create<T>(ReadOnlySpan<T[]> data) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length, default(T));
         public static Grid<T> Create<T>(ReadOnlySpan<T[]> data, T defaultValue) => new Grid<T>(CollectionUtil.Flatten(data), data.Length, data[0].Length, defaultValue);
         public static void WriteGrid(this Grid<char> grid, ConsoleWriter cw)
         {
@@ -32,12 +32,13 @@ namespace Kzrnm.Competitive
     }
     public class Grid<T>
     {
+        public int Size => data.Length;
         public readonly int H;
         public readonly int W;
         internal readonly T[] data;
         private readonly T defaultValue;
         public Grid(int H, int W, T defaultValue = default(T)) : this(new T[H * W].Fill(defaultValue), H, W, defaultValue) { }
-        internal Grid(T[] data, int H, int W, T defaultValue = default(T))
+        internal Grid(T[] data, int H, int W, T defaultValue)
         {
             this.H = H;
             this.W = W;
