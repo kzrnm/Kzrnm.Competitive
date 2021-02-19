d78ba8b036e36d0130aab9e9ee108ee862c12824
naminodarie <gengesa@gmail.com>
2021-02-19 19:07:05 +0900

    圧縮関数

--- Competitive.Library/Extensions/CollectionExtension.cs
+++ Competitive.Library/Extensions/CollectionExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using AtCoder.Internal;
 
 namespace System
 {
@@ -20,6 +21,28 @@ namespace System
             return v;
         }
 
+        /// <summary>
+        /// 連続する要素をひとまとめにした配列を返す。
+        /// </summary>
+        public static (T Value, int Count)[] CompressCount<T>(this IEnumerable<T> collection)
+        {
+            var e = collection.GetEnumerator();
+            var list = new SimpleList<(T Value, int Count)>();
+            if (!e.MoveNext()) return Array.Empty<(T, int)>();
+            var cur = e.Current;
+            list.Add((cur, 1));
+            while (e.MoveNext())
+            {
+                if (EqualityComparer<T>.Default.Equals(cur, e.Current))
+                    list[^1].Count++;
+                else
+                {
+                    cur = e.Current;
+                    list.Add((cur, 1));
+                }
+            }
+            return list.ToArray();
+        }
 
         public static T[] Flatten<T>(this T[][] array) => Flatten((ReadOnlySpan<T[]>)array);
         public static T[] Flatten<T>(this Span<T[]> span) => Flatten((ReadOnlySpan<T[]>)span);
