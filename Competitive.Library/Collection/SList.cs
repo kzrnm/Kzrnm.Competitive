ca5fcc93e0b065dd9e57a618640679b9ba9ed268
naminodarie <gengesa@gmail.com>
2021-02-24 00:20:45 +0900

    コンストラクタ

--- Competitive.Library/Collection/SList.cs
+++ Competitive.Library/Collection/SList.cs
@@ -1,9 +1,16 @@
-﻿using AtCoder.Internal;
+﻿using System.Collections.Generic;
+using AtCoder.Internal;
 
 namespace Kzrnm.Competitive
 {
     /// <summary>
     /// 機能を削った高速なArrayList
     /// </summary>
-    public class SList<T> : SimpleList<T> { }
+    public class SList<T> : SimpleList<T>
+    {
+        public SList() : base() { }
+        public SList(int capacity) : base(capacity) { }
+        public SList(IEnumerable<T> collection) : base(collection) { }
+
+    }
 }
