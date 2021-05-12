0d314251f3cfdd7379cc69604143928fa9cd8016
naminodarie <gengesa@gmail.com>
2021-05-13 02:47:06 +0900

    スライド最小値

--- Competitive.Library.Test/DataStructure/SlideGetTests.cs
+++ Competitive.Library.Test/DataStructure/SlideGetTests.cs
@@ -17,8 +17,10 @@ namespace Kzrnm.Competitive.DataStructure
         public void SlideMin()
         {
             var array = new int[] { 4, 1, 4, 6, 2, 3, 9, 7 };
-            var slide3 = SlideGet.Get(array, 3, new SlideMinOp());
-            slide3.Should().Equal(1, 1, 2, 2, 2, 3, 7, 7);
+            var slideFront3 = SlideGet.Front(array, 3, new SlideMinOp());
+            slideFront3.Should().Equal(4, 1, 1, 1, 2, 2, 2, 3);
+            var slideBack3 = SlideGet.Back(array, 3, new SlideMinOp());
+            slideBack3.Should().Equal(1, 1, 2, 2, 2, 3, 7, 7);
         }
 
         [Fact]
@@ -31,11 +33,17 @@ namespace Kzrnm.Competitive.DataStructure
             var st = new SparseTable<int, SlideMinOp>(array);
             for (int k = 1; k <= 30; k++)
             {
-                var slide = SlideGet.Get(array, k, new SlideMinOp());
-                var expected = new int[30];
-                for (int i = 0; i < expected.Length; i++)
-                    expected[i] = st[i..Math.Min(30, i + k)];
-                slide.Should().Equal(expected);
+                var slideFront = SlideGet.Front(array, k, new SlideMinOp());
+                var slideBack = SlideGet.Back(array, k, new SlideMinOp());
+                var expectedFront = new int[30];
+                var expectedBack = new int[30];
+                for (int i = 0; i < expectedBack.Length; i++)
+                {
+                    expectedFront[i] = st[Math.Max(0, i - k + 1)..(i + 1)];
+                    expectedBack[i] = st[i..Math.Min(30, i + k)];
+                }
+                slideFront.Should().Equal(expectedFront);
+                slideBack.Should().Equal(expectedBack);
             }
         }
     }
--- Competitive.Library/DataStructure/SlideGet.cs
+++ Competitive.Library/DataStructure/SlideGet.cs
@@ -19,10 +19,32 @@ namespace Kzrnm.Competitive
     public static class SlideGet
     {
         /// <summary>
+        /// <para><paramref name="array"/>(i-<paramref name="k"/>...i] で最も <typeparamref name="TOp"/> な値を返す。</para>
+        /// <para>計算量: O(<paramref name="array"/>.Length)</para>
+        /// </summary>
+        public static T[] Front<T, TOp>(T[] array, int k, TOp op = default)
+            where TOp : struct, SlideGetOperator<T>
+        {
+            AtCoder.Internal.Contract.Assert(k > 0, "k must be positive");
+            AtCoder.Internal.Contract.Assert(k <= array.Length, "k must not be greater than array.Length");
+            var result = new T[array.Length];
+            Deque<int> deq = new Deque<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                while (deq.Count > 0 && op.NeedUpdate(array[deq.Last], array[i]))
+                    deq.PopLast();
+                deq.AddLast(i);
+                var first = deq.First;
+                result[i] = array[first];
+                if (first == i - k + 1) deq.PopFirst();
+            }
+            return result;
+        }
+        /// <summary>
         /// <para><paramref name="array"/>[i...i+<paramref name="k"/>) で最も <typeparamref name="TOp"/> な値を返す。</para>
         /// <para>計算量: O(<paramref name="array"/>.Length)</para>
         /// </summary>
-        public static T[] Get<T, TOp>(T[] array, int k, TOp op = default)
+        public static T[] Back<T, TOp>(T[] array, int k, TOp op = default)
             where TOp : struct, SlideGetOperator<T>
         {
             AtCoder.Internal.Contract.Assert(k > 0, "k must be positive");
