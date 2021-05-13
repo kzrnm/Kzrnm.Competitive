a440c9750a5a20a4320df5b4a0f91bb990c9ed3b
naminodarie <gengesa@gmail.com>
2021-05-13 23:35:35 +0900

    SWAG

---
+++ Competitive.Library.Test/DataStructure/SWAGTests.cs
@@ -0,0 +1,47 @@
+﻿using AtCoder;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Kzrnm.Competitive.DataStructure
+{
+    public class SWAGTests
+    {
+        public struct SlideMinOp : ISegtreeOperator<int>
+        {
+            public int Identity => int.MaxValue;
+            public int Operate(int x, int y) => Math.Min(x, y);
+        }
+        [Fact]
+        public void SlideMin()
+        {
+            var array = new int[] { 4, 1, 4, 6, 2, 3, 9, 7 };
+            var swag = new SWAG<int, SlideMinOp>(array);
+            swag.Slide(0, 3).Should().Be(1);
+            swag.Slide(1, 4).Should().Be(1);
+            swag.Slide(2, 5).Should().Be(2);
+            swag.Slide(3, 6).Should().Be(2);
+            swag.Slide(4, 7).Should().Be(2);
+            swag.Slide(5, 8).Should().Be(3);
+            swag.Slide(6, 8).Should().Be(7);
+            swag.Slide(7, 8).Should().Be(7);
+
+            swag = new SWAG<int, SlideMinOp>(array);
+            swag.Slide(0, 1).Should().Be(4);
+            swag.Slide(0, 2).Should().Be(1);
+            swag.Slide(0, 3).Should().Be(1);
+            swag.Slide(0, 4).Should().Be(1);
+            swag.Slide(0, 5).Should().Be(1);
+            swag.Slide(0, 6).Should().Be(1);
+            swag.Slide(0, 7).Should().Be(1);
+            swag.Slide(0, 8).Should().Be(1);
+            swag.Slide(1, 8).Should().Be(1);
+            swag.Slide(2, 8).Should().Be(2);
+            swag.Slide(3, 8).Should().Be(2);
+            swag.Slide(4, 8).Should().Be(2);
+            swag.Slide(5, 8).Should().Be(3);
+            swag.Slide(6, 8).Should().Be(7);
+            swag.Slide(7, 8).Should().Be(7);
+        }
+    }
+}
---
+++ Competitive.Library/DataStructure/SWAG.cs
@@ -0,0 +1,55 @@
+﻿using AtCoder;
+using AtCoder.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kzrnm.Competitive
+{
+    /// <summary>
+    /// モノイドの範囲演算を移動しながら求める。計算量: O(N)
+    /// </summary>
+    public class SWAG<T, TOp> where TOp : struct, ISegtreeOperator<T>
+    {
+        private static TOp op = default;
+        private readonly T[] array;
+        private int left, right;
+        private T front;
+        private SimpleList<T> back;
+        public SWAG(T[] array)
+        {
+            this.array = array;
+            front = op.Identity;
+            back = new SimpleList<T>();
+        }
+        /// <summary>
+        /// <para>[<paramref name="l"/>..<paramref name="r"/>) の範囲クエリの結果を返す。</para>
+        /// <para>計算量: O(N)</para>
+        /// </summary>
+        public T Slide(int l, int r)
+        {
+            Contract.Assert(l <= r, "Range: l <= r");
+            Contract.Assert(left <= l, "左端は使用済みです");
+            Contract.Assert(right <= r, "右端は使用済みです");
+            Contract.Assert(l < array.Length, "左端が配列の範囲外です");
+            Contract.Assert(r <= array.Length, "右端が配列の範囲外です");
+            foreach (var v in array.AsSpan()[right..r])
+                front = op.Operate(front, v);
+            right = r;
+            while (left < l)
+            {
+                if (back.Count == 0)
+                {
+                    var tmp = op.Identity;
+                    for (int u = right - 1; u >= left; --u)
+                        back.Add(tmp = op.Operate(array[u], tmp));
+                    front = op.Identity;
+                }
+                back.RemoveLast();
+                ++left;
+            }
+            if (back.Count == 0) return front;
+            return op.Operate(back[^1], front);
+        }
+    }
+}
