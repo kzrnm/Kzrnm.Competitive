f163e1edde72bd2b891df3363c45c7a53045377c
naminodarie <gengesa@gmail.com>
2021-06-01 21:51:04 +0900

    01BFS

---
+++ Competitive.Library.Test/Graph/最短経路01BFSTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kzrnm.Competitive.Graph
+{
+    public class 最短経路01BFSTests
+    {
+        [Fact]
+        public void Int()
+        {
+            var gb = new WIntGraphBuilder(5, true);
+            gb.Add(0, 1, 1);
+            gb.Add(0, 2, 0);
+            gb.Add(0, 3, 1);
+            gb.Add(0, 4, 1);
+            gb.Add(1, 2, 0);
+            gb.Add(2, 3, 1);
+            gb.Add(2, 4, 0);
+            gb.Add(4, 3, 1);
+            gb.Add(4, 0, 1);
+            var graph = gb.ToGraph();
+            graph.ShortestPath01BFS(0).Should().Equal(new int[] { 0, 1, 0, 1, 0 });
+            graph.ShortestPath01BFS(1).Should().Equal(new int[] { 1, 0, 0, 1, 0 });
+            graph.ShortestPath01BFS(2).Should().Equal(new int[] { 1, 2, 0, 1, 0 });
+            graph.ShortestPath01BFS(3).Should().Equal(new int[] { int.MaxValue, int.MaxValue, int.MaxValue, 0, int.MaxValue });
+            graph.ShortestPath01BFS(4).Should().Equal(new int[] { 1, 2, 1, 1, 0 });
+        }
+
+        [Fact]
+        public void Long()
+        {
+            var gb = new WLongGraphBuilder(5, true);
+            gb.Add(0, 1, 1);
+            gb.Add(0, 2, 0);
+            gb.Add(0, 3, 1);
+            gb.Add(0, 4, 1);
+            gb.Add(1, 2, 0);
+            gb.Add(2, 3, 1);
+            gb.Add(2, 4, 0);
+            gb.Add(4, 3, 1);
+            gb.Add(4, 0, 1);
+            var graph = gb.ToGraph();
+            graph.ShortestPath01BFS(0).Should().Equal(new long[] { 0, 1, 0, 1, 0 });
+            graph.ShortestPath01BFS(1).Should().Equal(new long[] { 1, 0, 0, 1, 0 });
+            graph.ShortestPath01BFS(2).Should().Equal(new long[] { 1, 2, 0, 1, 0 });
+            graph.ShortestPath01BFS(3).Should().Equal(new long[] { long.MaxValue, long.MaxValue, long.MaxValue, 0, long.MaxValue });
+            graph.ShortestPath01BFS(4).Should().Equal(new long[] { 1, 2, 1, 1, 0 });
+        }
+    }
+}
---
+++ Competitive.Library/Graph/Utils/最短経路01BFS.cs
@@ -0,0 +1,60 @@
+﻿using AtCoder;
+using System.Collections.Generic;
+
+namespace Kzrnm.Competitive
+{
+    public static class 最短経路01BFS
+    {
+        /// <summary>
+        /// <para><paramref name="from"/> からの最短経路長を01-BSFで求める。</para>
+        /// <para>計算量: O(|E| + |V|)</para>
+        /// </summary>
+        public static T[] ShortestPath01BFS<T, TOp, TNode, TEdge>(this IWGraph<T, TOp, TNode, TEdge> graph, int from)
+            where T : struct
+            where TOp : struct, INumOperator<T>
+            where TNode : IGraphNode<TEdge>
+            where TEdge : IWGraphEdge<T>
+        {
+            TOp op = default;
+            var graphArr = graph.AsArray();
+            var INF = op.MaxValue;
+            var res = Global.NewArray(graphArr.Length, INF);
+            res[from] = default;
+
+            var used = new bool[graphArr.Length];
+            int count = 0;
+            var remains = new Deque<int>(graphArr.Length) { from };
+
+            while (remains.Count > 0)
+            {
+                var ix = remains.PopFirst();
+                if (used[ix]) continue;
+                used[ix] = true;
+                if (++count >= graphArr.Length) break;
+                var len = res[ix];
+                foreach (var e in graphArr[ix].Children)
+                {
+                    var nextLength = len;
+                    if (EqualityComparer<T>.Default.Equals(e.Value, default))
+                    {
+                        if (op.GreaterThan(res[e.To], nextLength))
+                        {
+                            res[e.To] = nextLength;
+                            remains.AddFirst(e.To);
+                        }
+                    }
+                    else
+                    {
+                        nextLength = op.Increment(nextLength);
+                        if (op.GreaterThan(res[e.To], nextLength))
+                        {
+                            res[e.To] = nextLength;
+                            remains.AddLast(e.To);
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
