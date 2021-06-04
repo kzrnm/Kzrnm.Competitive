6eda582547375b3dbc091e9b20c840b4ac6c82e5
naminodarie <gengesa@gmail.com>
2021-06-04 23:36:48 +0900

    ダブリング

---
+++ Competitive.Library.Test/DataStructure/PathDoublingTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Kzrnm.Competitive.DataStructure
+{
+    public class PathDoublingTests
+    {
+        [Fact]
+        public void Move()
+        {
+            var arr = new int[500];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = (i + 1) % arr.Length;
+            var rnd = new Random();
+            var db = new PathDoubling(arr);
+            Span<ulong> ks = stackalloc ulong[1];
+            for (int t = 0; t < 128; t++)
+            {
+                rnd.NextBytes(ks.Cast<ulong, byte>());
+                var k = ks[0];
+                for (int i = 0; i < 500; i++)
+                {
+                    db.Move(i, k).Should().Be((int)(((uint)i + k) % (uint)arr.Length));
+                    if ((long)k >= 0)
+                        db.Move(i, (long)k).Should().Be((int)(((uint)i + k) % (uint)arr.Length));
+                }
+            }
+        }
+
+        [Fact]
+        public void Tree()
+        {
+            var arr = new int[7]
+            {
+                -1,
+                0,
+                2,
+                1,
+                3,
+                2,
+                1,
+            };
+            var db = new PathDoubling(arr);
+            db.Move(0, 1).Should().Be(-1);
+            db.Move(1, 1).Should().Be(0);
+            db.Move(2, 1).Should().Be(2);
+            db.Move(3, 1).Should().Be(1);
+            db.Move(4, 1).Should().Be(3);
+            db.Move(5, 1).Should().Be(2);
+            db.Move(6, 1).Should().Be(1);
+
+            db.Move(0, 2).Should().Be(-1);
+            db.Move(1, 2).Should().Be(-1);
+            db.Move(2, 2).Should().Be(2);
+            db.Move(3, 2).Should().Be(0);
+            db.Move(4, 2).Should().Be(1);
+            db.Move(5, 2).Should().Be(2);
+            db.Move(6, 2).Should().Be(0);
+
+            db.Move(0, 3).Should().Be(-1);
+            db.Move(1, 3).Should().Be(-1);
+            db.Move(2, 3).Should().Be(2);
+            db.Move(3, 3).Should().Be(-1);
+            db.Move(4, 3).Should().Be(0);
+            db.Move(5, 3).Should().Be(2);
+            db.Move(6, 3).Should().Be(-1);
+
+            db.Move(0, 4).Should().Be(-1);
+            db.Move(1, 4).Should().Be(-1);
+            db.Move(2, 4).Should().Be(2);
+            db.Move(3, 4).Should().Be(-1);
+            db.Move(4, 4).Should().Be(-1);
+            db.Move(5, 4).Should().Be(2);
+            db.Move(6, 4).Should().Be(-1);
+
+            db.Move(0, 5).Should().Be(-1);
+            db.Move(1, 5).Should().Be(-1);
+            db.Move(2, 5).Should().Be(2);
+            db.Move(3, 5).Should().Be(-1);
+            db.Move(4, 5).Should().Be(-1);
+            db.Move(5, 5).Should().Be(2);
+            db.Move(6, 5).Should().Be(-1);
+
+            db.Move(0, 1L << 20).Should().Be(-1);
+            db.Move(1, 1L << 20).Should().Be(-1);
+            db.Move(2, 1L << 20).Should().Be(2);
+            db.Move(3, 1L << 20).Should().Be(-1);
+            db.Move(4, 1L << 20).Should().Be(-1);
+            db.Move(5, 1L << 20).Should().Be(2);
+            db.Move(6, 1L << 20).Should().Be(-1);
+        }
+    }
+}
---
+++ Competitive.Library/DataStructure/PathDoubling.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Kzrnm.Competitive
+{
+    /// <summary>
+    /// ダブリング: mod Nで循環しているなどのときに K 個先の遷移を logK で求められるデータ構造
+    /// </summary>
+    public class PathDoubling
+    {
+        /// <summary>
+        /// <para><paramref name="to"/>: インデックス i で数値 i からの遷移先。負数は遷移先なしとする。</para>
+        /// <para><paramref name="max"/>: 最大何番目までの遷移を見たいか</para>
+        /// <para>制約: <paramref name="to"/>[i] &lt; |<paramref name="to"/>|, 0 ≦ <paramref name="max"/></para>
+        /// </summary>
+        /// <param name="to">数値 i からの遷移先</param>
+        /// <param name="max">最大何番目までの遷移を見たいか</param>
+        public PathDoubling(int[] to, long max) : this(to, (ulong)max) { }
+        /// <summary>
+        /// <para><paramref name="to"/>: インデックス i で数値 i からの遷移先。負数は遷移先なしとする。</para>
+        /// <para><paramref name="max"/>: 最大何番目までの遷移を見たいか</para>
+        /// <para>制約: <paramref name="to"/>[i] &lt; |<paramref name="to"/>|</para>
+        /// </summary>
+        /// <param name="to">数値 i からの遷移先</param>
+        /// <param name="max">最大何番目までの遷移を見たいか</param>
+        public PathDoubling(int[] to, ulong max = ulong.MaxValue)
+        {
+            paths = new int[BitOperations.Log2(max) + 1][];
+            paths[0] = to;
+            for (int k = 0; k + 1 < paths.Length; k++)
+            {
+                var s = paths[k];
+                var t = paths[k + 1] = new int[s.Length];
+                for (int i = 0; i < s.Length; i++)
+                    if ((uint)s[i] < (uint)s.Length)
+                        t[i] = s[s[i]];
+                    else
+                        t[i] = -1;
+            }
+        }
+        private readonly int[][] paths;
+
+        /// <summary>
+        /// <para><paramref name="start"/>: スタートするインデックス</para>
+        /// <para><paramref name="moveNum"/>: 移動回数</para>
+        /// </summary>
+        /// <param name="start">: スタートするインデックス</param>
+        /// <param name="moveNum">: 移動回数</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Move(int start, long moveNum) => Move(start, (ulong)moveNum);
+        /// <summary>
+        /// <para><paramref name="start"/>: スタートするインデックス</para>
+        /// <para><paramref name="moveNum"/>: 移動回数</para>
+        /// </summary>
+        /// <param name="start">: スタートするインデックス</param>
+        /// <param name="moveNum">: 移動回数</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Move(int start, ulong moveNum)
+        {
+            int current = start;
+            foreach (var b in moveNum.Bits())
+            {
+                if ((uint)current >= (uint)paths[b].Length)
+                    return current;
+                current = paths[b][current];
+            }
+            return current;
+        }
+    }
+}
