b73b7a470022d0ab71c65c632bb571372429cedc
naminodarie <gengesa@gmail.com>
2021-06-28 03:53:27 +0900

    Update Library

--- Competitive.Library/Graph/Utils/最大流.cs
+++ Competitive.Library/Graph/Utils/最大流.cs
@@ -3,11 +3,11 @@ namespace Kzrnm.Competitive
 {
     public static class 最大流
     {
-        public static MFGraph<T, TOp> ToMFGraph<T, TOp>(this WGraphBuilder<T, TOp> gb)
+        public static MfGraph<T, TOp> ToMFGraph<T, TOp>(this WGraphBuilder<T, TOp> gb)
             where T : struct
             where TOp : struct, INumOperator<T>
         {
-            var mfg = new MFGraph<T, TOp>(gb.edgeContainer.Length);
+            var mfg = new MfGraph<T, TOp>(gb.edgeContainer.Length);
             foreach (var (i, e) in gb.edgeContainer.edges)
                 mfg.AddEdge(i, e.To, e.Value);
             return mfg;
