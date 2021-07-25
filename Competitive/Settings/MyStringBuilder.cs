b550b4720456f966dc1c2bc5f0d05610931f469b
naminodarie <gengesa@gmail.com>
2021-07-25 23:08:00 +0900

    Randomを上手く作りたい

--- Competitive/Settings/MyStringBuilder.cs
+++ Competitive/Settings/MyStringBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Competitive.Runner
@@ -14,6 +15,49 @@ namespace Competitive.Runner
         public MyStringBuilder Add(string s) { sb.AppendLine(s); return this; }
         public MyStringBuilder Add(params object[] objs) { sb.AppendLine(string.Join(" ", objs)); return this; }
         public MyStringBuilder Add<T>(IEnumerable<T> objs) { sb.AppendLine(string.Join(" ", objs)); return this; }
+        public MyStringBuilder AddRandom(long minValue, long maxValue)
+        {
+            // Originai is .NET runtime
+            // https://github.com/dotnet/runtime/blob/57bfe474518ab5b7cfe6bf7424a79ce3af9d6657/src/libraries/System.Private.CoreLib/src/System/Random.Xoshiro256StarStarImpl.cs
+            // LICENSE: https://github.com/dotnet/runtime/blob/main/LICENSE.TXT
+
+            static ulong UInt64Random()
+            {
+                Span<byte> resultBytes = stackalloc byte[8];
+                HandMadeMain.rnd.NextBytes(resultBytes);
+                return BitConverter.ToUInt64(resultBytes);
+            }
+            static int Log2Ceiling(ulong value)
+            {
+                int result = BitOperations.Log2(value);
+                if (BitOperations.PopCount(value) != 1)
+                {
+                    result++;
+                }
+                return result;
+            }
+            static long Int64Random(long minValue, long maxValue)
+            {
+                ulong exclusiveRange = (ulong)(maxValue - minValue);
+
+                if (exclusiveRange > 1)
+                {
+                    int bits = Log2Ceiling(exclusiveRange);
+                    while (true)
+                    {
+                        ulong result = UInt64Random() >> (sizeof(long) * 8 - bits);
+                        if (result < exclusiveRange)
+                        {
+                            return (long)result + minValue;
+                        }
+                    }
+                }
+
+                return minValue;
+            }
+            this.Add(Int64Random(minValue, maxValue).ToString());
+            return this;
+        }
         public MyStringBuilder Clear() { sb.Clear(); return this; }
         IEnumerator IEnumerable.GetEnumerator() { throw new NotSupportedException(); }
     }
