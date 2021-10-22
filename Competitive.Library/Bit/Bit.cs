5f29034f7e3b5f510ab7107c10dc288177a2e509
kzrnm <gengesa@gmail.com>
2021-10-22 18:27:50 +0900

    XML Docを追加

--- Competitive.Library/Bit/Bit.cs
+++ Competitive.Library/Bit/Bit.cs
@@ -10,21 +10,57 @@ namespace Kzrnm.Competitive
     using static MethodImplOptions;
     public static class Bit
     {
+        /// <summary>
+        /// <paramref name="num"/> を長さ <paramref name="padLeft"/> の 2 進数文字列にします。
+        /// </summary>
         public static string ToBitString(this int num, int padLeft = sizeof(int) * 8) => Convert.ToString(num, 2).PadLeft(padLeft, '0');
+        /// <summary>
+        /// <paramref name="num"/> を長さ <paramref name="padLeft"/> の 2 進数文字列にします。
+        /// </summary>
         public static string ToBitString(this uint num, int padLeft = sizeof(uint) * 8) => Convert.ToString((int)num, 2).PadLeft(padLeft, '0');
+        /// <summary>
+        /// <paramref name="num"/> を長さ <paramref name="padLeft"/> の 2 進数文字列にします。
+        /// </summary>
         public static string ToBitString(this long num, int padLeft = sizeof(long) * 8) => Convert.ToString(num, 2).PadLeft(padLeft, '0');
+        /// <summary>
+        /// <paramref name="num"/> を長さ <paramref name="padLeft"/> の 2 進数文字列にします。
+        /// </summary>
         public static string ToBitString(this ulong num, int padLeft = sizeof(ulong) * 8) => Convert.ToString(unchecked((long)num), 2).PadLeft(padLeft, '0');
+        /// <summary>
+        /// <paramref name="num"/> の <paramref name="index"/> 番目のビットが立っているかを返します。
+        /// </summary>
         [MethodImpl(AggressiveInlining)]
         public static bool On(this int num, int index) => ((num >> index) & 1) != 0;
+        /// <summary>
+        /// <paramref name="num"/> の <paramref name="index"/> 番目のビットが立っているかを返します。
+        /// </summary>
         [MethodImpl(AggressiveInlining)]
         public static bool On(this uint num, int index) => ((num >> index) & 1) != 0;
+        /// <summary>
+        /// <paramref name="num"/> の <paramref name="index"/> 番目のビットが立っているかを返します。
+        /// </summary>
         [MethodImpl(AggressiveInlining)]
         public static bool On(this long num, int index) => ((num >> index) & 1) != 0;
+        /// <summary>
+        /// <paramref name="num"/> の <paramref name="index"/> 番目のビットが立っているかを返します。
+        /// </summary>
         [MethodImpl(AggressiveInlining)]
         public static bool On(this ulong num, int index) => ((num >> index) & 1) != 0;
+        /// <summary>
+        /// <paramref name="num"/> の立っているビットを列挙します。
+        /// </summary>
         public static Enumerator Bits(this int num) => new Enumerator(num);
+        /// <summary>
+        /// <paramref name="num"/> の立っているビットを列挙します。
+        /// </summary>
         public static Enumerator Bits(this uint num) => new Enumerator(num);
+        /// <summary>
+        /// <paramref name="num"/> の立っているビットを列挙します。
+        /// </summary>
         public static Enumerator Bits(this long num) => new Enumerator(num);
+        /// <summary>
+        /// <paramref name="num"/> の立っているビットを列挙します。
+        /// </summary>
         public static Enumerator Bits(this ulong num) => new Enumerator(num);
         public struct Enumerator : IEnumerable<int>, IEnumerator<int>
         {
