c7699d2c1fc872c1877bd77347adf5702f2a4886
kzrnm <gengesa@gmail.com>
2022-03-08 20:06:08 +0900

    ToArray

--- Competitive.Library/DataStructure/BIT/FenwickTreeExtension.cs
+++ Competitive.Library/DataStructure/BIT/FenwickTreeExtension.cs
@@ -1,4 +1,5 @@
 ﻿using AtCoder;
+using AtCoder.Internal;
 using AtCoder.Operators;
 using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
 
@@ -56,6 +57,29 @@ namespace Kzrnm.Competitive
                 }
             }
             return x;
+        }
+
+        /// <summary>
+        /// <c>(<paramref name="fw"/>[i..(i+1)], <paramref name="fw"/>[..(i+1)])</c> の配列にして返す。
+        /// </summary>
+        /// <returns></returns>
+        [凾(256)]
+        public static (TValue Item, TValue Sum)[] ToArray<TValue, TOp>(this FenwickTree<TValue, TOp> fw)
+            where TOp : struct, IAdditionOperator<TValue>, ISubtractOperator<TValue>
+        {
+            var op = default(TOp);
+            var data = fw.data;
+            var items = new (TValue Item, TValue Sum)[data.Length - 1];
+            items[0] = (data[1], data[1]);
+            for (int i = 2; i < data.Length; i++)
+            {
+                int length = (int)InternalBit.ExtractLowestSetBit(i);
+                var pr = i - length - 1;
+                var sum = op.Add(data[i], 0 <= pr ? items[pr].Sum : default);
+                var val = op.Subtract(sum, items[i - 2].Sum);
+                items[i - 1] = (val, sum);
+            }
+            return items;
         }
     }
 }
