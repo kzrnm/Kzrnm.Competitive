3c72c5d2dd68387e68d66b43f0f9b65a25a8f91d
kzrnm <gengesa@gmail.com>
2022-03-21 02:59:31 +0900

    行列式のtest

---
+++ Library/LibraryChecker/Math/MatrixDeterminant.test.cs
@@ -0,0 +1,18 @@
+﻿using Kzrnm.Competitive.IO;
+using ModInt = AtCoder.StaticModInt<AtCoder.Mod998244353>;
+using ModIntOperator = AtCoder.StaticModIntOperator<AtCoder.Mod998244353>;
+
+namespace Kzrnm.Competitive.MathNs
+{
+    public class MatrixDeterminantTest
+    {
+        static void Main() { using var cw = new Utf8ConsoleWriter(); Solve(new ConsoleReader(), cw); }
+        // verification-helper: PROBLEM https://judge.yosupo.jp/problem/matrix_det
+        static void Solve(ConsoleReader cr, Utf8ConsoleWriter cw)
+        {
+            int N = cr;
+            var mat = new ArrayMatrix<ModInt, ModIntOperator>(cr.Repeat(N).Select(cr => cr.Repeat(N).Select(cr => ModInt.Raw(cr))));
+            cw.WriteLine(mat.Determinant().Value);
+        }
+    }
+}
