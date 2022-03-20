af34a7df205a8e0317794c9d636d2bd2fc759c52
kzrnm <gengesa@gmail.com>
2022-03-20 21:17:32 +0900

    Split ConsoleOutput

--- Library/Competitive.Library/Program.cs
+++ Library/Competitive.Library/Program.cs
@@ -22,33 +22,6 @@ internal partial class Program
         cw.Flush();
     }
 }
-namespace Kzrnm.Competitive
-{
-    public static class ConsoleOutputExt
-    {
-        public static ConsoleOutput ToConsoleOutput(this IUtf8ConsoleWriterFormatter f)
-        {
-            var cw = ConsoleOutput.cw;
-            f.Write(cw);
-            return cw.WriteLine();
-        }
-    }
-    public struct ConsoleOutput
-    {
-        public static Utf8ConsoleWriter cw;
-        public static implicit operator ConsoleOutput(int v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(long v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(uint v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(ulong v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(double v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(decimal v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(char v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(char[] v) => cw.WriteLine((ReadOnlySpan<char>)v);
-        public static implicit operator ConsoleOutput(string v) => cw.WriteLine((ReadOnlySpan<char>)v);
-        public static implicit operator ConsoleOutput(bool v) => cw.WriteLine((ReadOnlySpan<char>)Program.YesNo(v));
-        public static implicit operator ConsoleOutput(Utf8ConsoleWriter _) => default;
-    }
-}
 #if !LIBRARY
 [SourceExpander.NotEmbeddingSource]
 #endif
