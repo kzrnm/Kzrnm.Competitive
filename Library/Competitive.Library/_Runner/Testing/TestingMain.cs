93e88789e5b435e79910493e65ebe6cb1ced1c43
kzrnm <gengesa@gmail.com>
2022-03-22 21:45:18 +0900

    Testing library

---
+++ Library/Competitive.Library/_Runner/Testing/TestingMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Kzrnm.Competitive.Testing
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class VerifyAttribute : Attribute
+    {
+        /// <summary>
+        /// TestingMain を埋め込むための Attribute
+        /// </summary>
+        public VerifyAttribute() { }
+    }
+    public static class TestingMain
+    {
+        public static void Main()
+            => Console.WriteLine(Console.ReadLine().Split().Sum(int.Parse));
+    }
+}
