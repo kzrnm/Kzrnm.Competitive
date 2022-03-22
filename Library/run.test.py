9fc659d42620e587c65a5c1591665030a06cf224
kzrnm <gengesa@gmail.com>
2022-03-23 02:50:21 +0900

    Redirect output of dotnet test to stderr

--- Library/run.test.py
+++ Library/run.test.py
@@ -1,13 +1,15 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
 from pathlib import Path
 import subprocess
+import sys
 
 
 def main():
+    print('Hello World')
     thisfile = Path(__file__).resolve()
     subprocess.run([
-        'dotnet', 'test',
-        str(thisfile.parent / 'Competitive.Library.sln')])
+        'dotnet', 'test', str(thisfile.parent / 'Competitive.Library.sln')],
+        stdout=sys.stderr)
 
 
 if __name__ == '__main__':
