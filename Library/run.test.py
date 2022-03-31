9665b19ea9f7377fa9f76cf31681a7488241435b
kzrnm <gengesa@gmail.com>
2022-04-01 02:43:32 +0900

    run.py

--- Library/run.test.py
+++ Library/run.test.py
@@ -5,7 +5,7 @@ import subprocess
 
 def main():
     sln_path = str(Path(__file__).resolve().parent / 'Competitive.Library.sln')
-    subprocess.run(['dotnet', 'test', sln_path], check=True)  # run
+    subprocess.run(['dotnet', 'test', sln_path], check=True)
 
 
 if __name__ == '__main__':
