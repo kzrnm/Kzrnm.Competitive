b9d189bd9217616e443ae3b925eeddad1104e8aa
kzrnm <gengesa@gmail.com>
2022-03-31 23:15:20 +0900

    エイリアスで短く

--- Library/Competitive.Library/_Runner/ConsoleOutput.cs
+++ Library/Competitive.Library/_Runner/ConsoleOutput.cs
@@ -3,11 +3,12 @@ using System;
 
 namespace Kzrnm.Competitive
 {
+    using O = ConsoleOutput;
     public static class ConsoleOutputExt
     {
-        public static ConsoleOutput ToConsoleOutput<T>(this T f) where T : IUtf8ConsoleWriterFormatter
+        public static O ToConsoleOutput<T>(this T f) where T : IUtf8ConsoleWriterFormatter
         {
-            var cw = ConsoleOutput.cw;
+            var cw = O.cw;
             f.Write(cw);
             return cw.WriteLine();
         }
@@ -15,17 +16,17 @@ namespace Kzrnm.Competitive
     public struct ConsoleOutput
     {
         public static Utf8ConsoleWriter cw;
-        public static implicit operator ConsoleOutput(int v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(long v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(uint v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(ulong v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(double v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(decimal v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(char v) => cw.WriteLine(v);
-        public static implicit operator ConsoleOutput(char[] v) => cw.WriteLine((ReadOnlySpan<char>)v);
-        public static implicit operator ConsoleOutput(string v) => cw.WriteLine((ReadOnlySpan<char>)v);
-        public static implicit operator ConsoleOutput(bool v) => cw.WriteLine((ReadOnlySpan<char>)Program.YesNo(v));
-        public static implicit operator ConsoleOutput(Utf8ConsoleWriter _) => default;
+        public static implicit operator O(int v) => cw.WriteLine(v);
+        public static implicit operator O(long v) => cw.WriteLine(v);
+        public static implicit operator O(uint v) => cw.WriteLine(v);
+        public static implicit operator O(ulong v) => cw.WriteLine(v);
+        public static implicit operator O(double v) => cw.WriteLine(v);
+        public static implicit operator O(decimal v) => cw.WriteLine(v);
+        public static implicit operator O(char v) => cw.WriteLine(v);
+        public static implicit operator O(char[] v) => cw.WriteLine((ReadOnlySpan<char>)v);
+        public static implicit operator O(string v) => cw.WriteLine((ReadOnlySpan<char>)v);
+        public static implicit operator O(bool v) => cw.WriteLine((ReadOnlySpan<char>)Program.YesNo(v));
+        public static implicit operator O(Utf8ConsoleWriter _) => default;
     }
 }
 // @brief 出力用のラッパー
