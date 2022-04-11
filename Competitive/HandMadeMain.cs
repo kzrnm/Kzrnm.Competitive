cb7b428d3ce7be319cf0b6cc45ac67c36265d443
kzrnm <gengesa@gmail.com>
2022-04-12 03:48:13 +0900

    random tree

--- Competitive/HandMadeMain.cs
+++ Competitive/HandMadeMain.cs
@@ -14,7 +14,6 @@ namespace Competitive.Runner
     {
         static string BasePath = CurrentPath();
         internal static Random rnd = new Random(227);
-        private static int[] RandomArray(int length, int min, int maxExclusive) { var arr = new int[length]; for (int i = 0; i < arr.Length; i++) arr[i] = rnd.Next(min, maxExclusive); return arr; }
         static MyStringBuilder Build()
         {
             var sb = new MyStringBuilder
@@ -22,6 +21,19 @@ namespace Competitive.Runner
 
             };
 
+            return sb;
+        }
+    }
+
+    // Utilメソッド
+    static partial class HandMadeMain
+    {
+        static int[] RandomArray(int length, int min, int maxExclusive) { var arr = new int[length]; for (int i = 0; i < arr.Length; i++) arr[i] = rnd.Next(min, maxExclusive); return arr; }
+        static MyStringBuilder BuildRandomTree(MyStringBuilder sb, int size)
+        {
+            sb.Add(size);
+            for (int i = 2; i <= size; i++)
+                sb.Add(i, rnd.Next(1, i));
             return sb;
         }
     }
