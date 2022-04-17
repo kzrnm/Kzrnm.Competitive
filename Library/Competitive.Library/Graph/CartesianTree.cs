799b636cae71642914b6238eb8d0cd125405f15c
kzrnm <gengesa@gmail.com>
2022-04-17 15:38:41 +0900

    generics

--- Library/Competitive.Library/Graph/CartesianTree.cs
+++ Library/Competitive.Library/Graph/CartesianTree.cs
@@ -14,7 +14,18 @@ namespace Kzrnm.Competitive
         /// <para>計算量: O(N)</para>
         /// </remarks>
         /// <returns>親のインデックス(根は -1 )を保持する配列</returns>
-        public static int[] Create(int[] a, IComparer<int> comparer = null)
+        public static int[] Create<T>(T[] a) where T : IComparable<T> => Create(a, new DefaultComparerStruct<T>());
+
+        /// <summary>
+        /// <para>Cartesian tree を構築します。</para>
+        /// <para>Cartesian tree: 最も小さい値を根として、左右それぞれで根の次に大きいものを子とする二分木</para>
+        /// <para>https://en.wikipedia.org/wiki/Cartesian_tree</para>
+        /// </summary>
+        /// <remarks>
+        /// <para>計算量: O(N)</para>
+        /// </remarks>
+        /// <returns>親のインデックス(根は -1 )を保持する配列</returns>
+        public static int[] Create<T, TCmp>(T[] a, TCmp comparer) where TCmp : IComparer<T>
         {
             var parents = new int[a.Length];
             parents.AsSpan().Fill(-1);
@@ -24,10 +35,7 @@ namespace Kzrnm.Competitive
                 int p = i - 1;
                 int l = -1;
 
-                while (p != -1 && (
-                    comparer == null
-                    ? a[i].CompareTo(a[p])
-                    : comparer.Compare(a[i], a[p])) < 0)
+                while (p != -1 && comparer.Compare(a[i], a[p]) < 0)
                 {
                     int pp = parents[p];
                     if (l != -1)
