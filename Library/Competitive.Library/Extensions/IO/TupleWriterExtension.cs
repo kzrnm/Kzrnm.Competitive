9cbeb81384223df5d97b5ae899acdb3431d0954b
kzrnm <gengesa@gmail.com>
2022-05-16 22:11:59 +0900

    タプルを出力

---
+++ Library/Competitive.Library/Extensions/IO/TupleWriterExtension.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace Kzrnm.Competitive.IO
+{
+    public static class __Utf8ConsoleWriter__TupleWriterExtension
+    {
+        public static Utf8ConsoleWriter WriteTuples<T1, T2>(this Utf8ConsoleWriter cw, ReadOnlySpan<(T1, T2)> tuple)
+        {
+            foreach (var t in tuple)
+                cw.WriteLineJoin(t);
+            return cw;
+        }
+        public static Utf8ConsoleWriter WriteTuples<T1, T2>(this Utf8ConsoleWriter cw, IEnumerable<(T1, T2)> tuple)
+        {
+            foreach (var t in tuple)
+                cw.WriteLineJoin(t);
+            return cw;
+        }
+        public static Utf8ConsoleWriter WriteTuples<T1, T2, T3>(this Utf8ConsoleWriter cw, ReadOnlySpan<(T1, T2, T3)> tuple)
+        {
+            foreach (var t in tuple)
+                cw.WriteLineJoin(t);
+            return cw;
+        }
+        public static Utf8ConsoleWriter WriteTuples<T1, T2, T3>(this Utf8ConsoleWriter cw, IEnumerable<(T1, T2, T3)> tuple)
+        {
+            foreach (var t in tuple)
+                cw.WriteLineJoin(t);
+            return cw;
+        }
+        public static Utf8ConsoleWriter WriteTuples<T1, T2, T3, T4>(this Utf8ConsoleWriter cw, ReadOnlySpan<(T1, T2, T3, T4)> tuple)
+        {
+            foreach (var t in tuple)
+                cw.WriteLineJoin(t);
+            return cw;
+        }
+        public static Utf8ConsoleWriter WriteTuples<T1, T2, T3, T4>(this Utf8ConsoleWriter cw, IEnumerable<(T1, T2, T3, T4)> tuple)
+        {
+            foreach (var t in tuple)
+                cw.WriteLineJoin(t);
+            return cw;
+        }
+    }
+}
