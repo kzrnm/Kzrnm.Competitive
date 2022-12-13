c161f31df68319dc19753daa808c985cd0cd22bd
kzrnm <gengesa@gmail.com>
2022-12-14 00:06:49 +0900

    PriorityQueue

---
+++ Library/Competitive.Library.Test/Collection/PriorityQueueTest.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+
+namespace Kzrnm.Competitive.Testing.Collection
+{
+    public class PriorityQueueTest
+    {
+        [Fact]
+        public void TryDequeue()
+        {
+            var pq = PriorityQueue.CreateDictionary<int, (string A, string B)>();
+
+            pq.Enqueue(1, ("A", "E"));
+            pq.Enqueue(5, ("B", "F"));
+            pq.Enqueue(4, ("C", "G"));
+            pq.Enqueue(2, ("D", "H"));
+
+            pq.TryDequeue(out var key, out var a, out var b).Should().BeTrue();
+            key.Should().Be(1); a.Should().Be("A"); b.Should().Be("E");
+            pq.TryDequeue(out key, out a, out b).Should().BeTrue();
+            key.Should().Be(2); a.Should().Be("D"); b.Should().Be("H");
+            pq.TryDequeue(out key, out a, out b).Should().BeTrue();
+            key.Should().Be(4); a.Should().Be("C"); b.Should().Be("G");
+            pq.TryDequeue(out key, out a, out b).Should().BeTrue();
+            key.Should().Be(5); a.Should().Be("B"); b.Should().Be("F");
+
+            pq.TryDequeue(out _, out _, out _).Should().BeFalse();
+        }
+    }
+}
---
+++ Library/Competitive.Library/Collection/PriorityQueue.md
@@ -0,0 +1,10 @@
+---
+title: AtCoder.PriorityQueue の便利メソッド
+documentation_of: ./PriorityQueue.cs
+---
+
+## 概要
+
+コンストラクタより `Create` の方が作りやすそう。
+
+タプルの `TryDequeue` は地味に便利。
