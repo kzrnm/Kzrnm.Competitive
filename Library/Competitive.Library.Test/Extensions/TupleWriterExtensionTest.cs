f06f76a318f506b96e6a18b75cab9b98a8410153
kzrnm <gengesa@gmail.com>
2022-12-14 22:16:32 +0900

    TupleWriterExtensionTest

---
+++ Library/Competitive.Library.Test/Extensions/TupleWriterExtensionTest.cs
@@ -0,0 +1,105 @@
+using Kzrnm.Competitive.IO;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kzrnm.Competitive.Testing.IO
+{
+    public class TupleWriterExtensionTest
+    {
+        private class Utf8Wrapper
+        {
+            readonly MemoryStream stream = new();
+            public static implicit operator Stream(Utf8Wrapper writer) => writer.stream;
+            public string Read() => new UTF8Encoding(false).GetString(stream.ToArray());
+        }
+
+        [Fact]
+        public void WriteTuples2()
+        {
+            var utf8Wrapper = new Utf8Wrapper();
+            using (var cw = new Utf8ConsoleWriter(utf8Wrapper))
+            {
+                var arr = new (int Num, string Text)[]
+                {
+                    (1, "Red"),
+                    (3, "Blue"),
+                    (5, "Green"),
+                    (7, "White"),
+                };
+                cw.WriteTuples<int, string>(arr.AsSpan());
+                cw.WriteTuples(arr.AsEnumerable().Reverse());
+            }
+            utf8Wrapper.Read().Should().Be("""
+            1 Red
+            3 Blue
+            5 Green
+            7 White
+            7 White
+            5 Green
+            3 Blue
+            1 Red
+
+            """.Replace("\r\n", "\n"));
+        }
+
+        [Fact]
+        public void WriteTuples3()
+        {
+            var utf8Wrapper = new Utf8Wrapper();
+            using (var cw = new Utf8ConsoleWriter(utf8Wrapper))
+            {
+                var arr = new (int Num, string Text, char C)[]
+                {
+                    (1, "Red", 'r'),
+                    (3, "Blue", 'b'),
+                    (5, "Green", 'g'),
+                    (7, "White", 'w'),
+                };
+                cw.WriteTuples<int, string, char>(arr.AsSpan());
+                cw.WriteTuples(arr.AsEnumerable().Reverse());
+            }
+            utf8Wrapper.Read().Should().Be("""
+            1 Red r
+            3 Blue b
+            5 Green g
+            7 White w
+            7 White w
+            5 Green g
+            3 Blue b
+            1 Red r
+
+            """.Replace("\r\n", "\n"));
+        }
+
+        [Fact]
+        public void WriteTuples4()
+        {
+            var utf8Wrapper = new Utf8Wrapper();
+            using (var cw = new Utf8ConsoleWriter(utf8Wrapper))
+            {
+                var arr = new (int Num, string Text, char C, decimal D)[]
+                {
+                    (1, "Red", 'r', 0.5m),
+                    (3, "Blue", 'b', 0.75m),
+                    (5, "Green", 'g', 0.25m),
+                    (7, "White", 'w', 0.3m),
+                };
+                cw.WriteTuples<int, string, char,decimal>(arr.AsSpan());
+                cw.WriteTuples(arr.AsEnumerable().Reverse());
+            }
+            utf8Wrapper.Read().Should().Be("""
+            1 Red r 0.50000000000000000000
+            3 Blue b 0.75000000000000000000
+            5 Green g 0.25000000000000000000
+            7 White w 0.30000000000000000000
+            7 White w 0.30000000000000000000
+            5 Green g 0.25000000000000000000
+            3 Blue b 0.75000000000000000000
+            1 Red r 0.50000000000000000000
+
+            """.Replace("\r\n", "\n"));
+        }
+    }
+}
--- Library/Competitive.Library/Extensions/IO/TupleWriterExtension.cs
+++ Library/Competitive.Library/Extensions/IO/TupleWriterExtension.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Kzrnm.Competitive.IO
 {
+    // competitive-verifier: TITLE TupleWriterExtension
     public static class __Utf8ConsoleWriter__TupleWriterExtension
     {
         public static Utf8ConsoleWriter WriteTuples<T1, T2>(this Utf8ConsoleWriter cw, ReadOnlySpan<(T1, T2)> tuple)
