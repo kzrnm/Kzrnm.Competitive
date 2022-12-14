c990ad779731b82179ac98775adab543dee3e9bf
kzrnm <gengesa@gmail.com>
2022-12-14 21:38:14 +0900

    Trim BitDebug

--- Library/Competitive.Library/Util/BitDebug.cs
+++ Library/Competitive.Library/Util/BitDebug.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 
 namespace Kzrnm.Competitive
 {
+    using static DebuggerBrowsableState;
     /// <summary>
     /// 配列のインデックスを 2 進数表記でデバッガのウォッチ式に表示します。
     /// </summary>
@@ -11,10 +12,10 @@ namespace Kzrnm.Competitive
     {
         public BitDebug(Array array)
         {
-            Array = array;
+            a = array;
         }
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        public Array Array;
+        [DebuggerBrowsable(Never)]
+        Array a;
 
 
         [DebuggerDisplay("{" + nameof(value) + ",nq}", Name = "{" + nameof(key) + ",nq}")]
@@ -22,24 +23,24 @@ namespace Kzrnm.Competitive
         {
             public DebugItem(int index, int len, object value)
             {
-                this.index = index;
                 key = $"{Convert.ToString(index, 2).PadLeft(len, '0')} [{index}]";
                 this.value = value;
             }
-            public readonly string key;
-            public readonly int index;
-            public readonly object value;
+            [DebuggerBrowsable(Never)]
+            string key;
+            [DebuggerBrowsable(RootHidden)]
+            object value;
         }
-        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        [DebuggerBrowsable(RootHidden)]
         public DebugItem[] Items
         {
             get
             {
-                var items = new DebugItem[Array.Length];
-                var len = BitOperations.Log2((uint)Array.Length - 1) + 1;
+                var items = new DebugItem[a.Length];
+                var len = BitOperations.Log2((uint)a.Length - 1) + 1;
                 for (int i = 0; i < items.Length; i++)
                 {
-                    items[i] = new DebugItem(i, len, Array.GetValue(i));
+                    items[i] = new DebugItem(i, len, a.GetValue(i));
                 }
                 return items;
             }
