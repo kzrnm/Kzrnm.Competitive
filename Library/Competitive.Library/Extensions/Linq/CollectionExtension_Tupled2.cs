d4153afc5b845c17aaa5a13677b72362fb852ebc
kzrnm <gengesa@gmail.com>
2022-12-15 14:10:12 +0900

    タプルの変数名

--- Library/Competitive.Library/Extensions/Linq/CollectionExtension_Tupled2.cs
+++ Library/Competitive.Library/Extensions/Linq/CollectionExtension_Tupled2.cs
@@ -3,7 +3,7 @@ using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
 
 namespace Kzrnm.Competitive
 {
-    // competitive-verifier: TITLE (T, T)[] への変換
+    // competitive-verifier: TITLE (T Current, T Next)[] への変換
     public static class __CollectionExtension_Tupled2
     {
         /// <summary>
