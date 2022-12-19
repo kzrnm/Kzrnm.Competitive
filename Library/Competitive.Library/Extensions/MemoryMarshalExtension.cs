70ea1bb0b888861bf0fee1e21978d54387213217
kzrnm <gengesa@gmail.com>
2022-12-20 02:54:31 +0900

    GetValueRefOrAddDefault

--- Library/Competitive.Library/Extensions/MemoryMarshalExtension.cs
+++ Library/Competitive.Library/Extensions/MemoryMarshalExtension.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
+#if !NET7_0_OR_GREATER
+using System.Runtime.CompilerServices;
+#endif
 
 namespace Kzrnm.Competitive
 {
@@ -12,7 +14,21 @@ namespace Kzrnm.Competitive
         private class ArrayVal<T> { public T[] arr; }
 #pragma warning restore CS0649
         [凾(256)]
-        public static Span<T> AsSpan<T>(this List<T> list, int start = 0) => Unsafe.As<ArrayVal<T>>(list).arr.AsSpan(start, list.Count - start);
+        public static Span<T> AsSpan<T>(this List<T> list, int start = 0)
+#if NET7_0_OR_GREATER
+            => CollectionsMarshal.AsSpan(list)[start..];
+#else
+            => Unsafe.As<ArrayVal<T>>(list).arr.AsSpan(start, list.Count - start);
+#endif
+
+#if NET7_0_OR_GREATER
+        [凾(256)]
+        public static ref TValue GetValueRefOrAddDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+            => ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out _);
+        [凾(256)]
+        public static ref TValue GetValueRefOrAddDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, out bool exists)
+            => ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out exists);
+#endif
         [凾(256)]
         public static Span<TTo> Cast<TFrom, TTo>(this Span<TFrom> span)
             where TFrom : struct
