9d79d84c740bffd0d821bdf0db5e2109d5667566
kzrnm <gengesa@gmail.com>
2023-01-01 21:01:12 +0900

    BitArrayの拡張メソッド

--- Library/Competitive.Library.Test/Extensions/BitArrayExtensionTest.cs
+++ Library/Competitive.Library.Test/Extensions/BitArrayExtensionTest.cs
@@ -1,10 +1,26 @@
 using System;
 using System.Collections;
+using System.Linq;
 
 namespace Kzrnm.Competitive.Testing.Extensions
 {
     public class BitArrayExtensionTests
     {
+        [Fact]
+        public void GetUnsageIntArray()
+        {
+            var b = new BitArray(70);
+            b.SetAll(true);
+            b[0] = false;
+            b[64] = false;
+
+            var arr = b.GetArray();
+            arr.Should().HaveCount(3)
+                .And
+                .StartWith(new int[] { -2, -1 });
+            (arr[2] & 0b111111).Should().Be(0b111110);
+        }
+
         [Fact]
         public void EmptyCopyTo()
         {
@@ -88,13 +104,13 @@ namespace Kzrnm.Competitive.Testing.Extensions
         [Fact]
         public void OnBitRandom()
         {
-            var rnd = new Random();
+            var rnd = new Random(227);
             for (int len = 1; len < 5000; len *= 3)
             {
                 var brr = new byte[len];
                 rnd.NextBytes(brr);
                 var b = new BitArray(brr);
-                var bbs = b.OnBits().AsSpan();
+                var bbs = b.OnBits().ToArray().AsSpan();
                 for (int i = 0; i < b.Length; i++)
                 {
                     if (bbs.Length > 0 && bbs[0] == i)
@@ -107,6 +123,59 @@ namespace Kzrnm.Competitive.Testing.Extensions
                 }
                 bbs.Length.Should().Be(0);
             }
+        }
+
+        [Theory]
+        [InlineData(63)]
+        [InlineData(64)]
+        [InlineData(65)]
+        [InlineData(100)]
+        public void PopCount(int size)
+        {
+            var rnd = new Random(227);
+            var b = new BitArray(size);
+            b.PopCount().Should().Be(0);
+            b[size - 1] = true;
+            b.PopCount().Should().Be(1);
+            for (int n = 0; n < 300; n++)
+            {
+                var ix = rnd.Next(size);
+                b[ix] = true;
+                var c = 0;
+                for (int i = 0; i < size; i++)
+                    if (b[i])
+                        ++c;
+                b.PopCount().Should().Be(c);
+            }
+        }
+
+        public static TheoryData LeadingZeroCount_Data = new TheoryData<BitArray, int>
+        {
+            {
+                new BitArray(
+                    Enumerable.Repeat(false,16).Concat(
+                        Enumerable.Repeat(true,2)
+                    ).ToArray()
+                ), 16
+            },
+            {
+                new BitArray(
+                    Enumerable.Repeat(false,16).Concat(
+                        Enumerable.Repeat(true,2)
+                    ).Concat(
+                        Enumerable.Repeat(false,2)
+                    ).Concat(
+                        Enumerable.Repeat(true,2)
+                    ).ToArray()
+                ), 16
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(LeadingZeroCount_Data))]
+        public void LeadingZeroCount(BitArray b, int lsb)
+        {
+            b.LeadingZeroCount().Should().Be(lsb);
         }
     }
 }
--- Library/Competitive.Library/Extensions/BitArrayExtension.cs
+++ Library/Competitive.Library/Extensions/BitArrayExtension.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
 
@@ -8,6 +10,17 @@ namespace Kzrnm.Competitive
 {
     public static class __BitArrayExtension
     {
+#pragma warning disable CS0649
+        private class Dummy
+        {
+            public int[] arr;
+            public int len;
+            public int ver;
+        }
+#pragma warning restore CS0649
+        [凾(256)]
+        public static int[] GetArray(this BitArray b) => Unsafe.As<Dummy>(b).arr;
+
         [凾(256)]
         public static void CopyTo(this BitArray b, int[] array, int index = 0) => b.CopyTo((Array)array, index);
         [凾(256)]
@@ -31,15 +44,60 @@ namespace Kzrnm.Competitive
         [凾(256)]
         public static int[] OnBits(this BitArray b)
         {
-            var arr = new uint[((b.Length + 63) >> 6) * 2];
-            if (arr.Length == 0) return Array.Empty<int>();
-            b.CopyTo(arr);
-            var brr = MemoryMarshal.Cast<uint, ulong>(arr);
+            var arr = b.GetArray();
+            var brr = MemoryMarshal.Cast<int, ulong>(arr);
             var l = new List<int>(b.Length);
             for (var i = 0; i < brr.Length; ++i)
                 foreach (var bit in brr[i].Bits())
-                    l.Add(bit + 64 * i);
+                {
+                    var bb = bit + 64 * i;
+                    if (bb >= b.Length) break;
+                    l.Add(bb);
+                }
+            if ((arr.Length & 1) != 0)
+                foreach (var bit in arr[^1].Bits())
+                {
+                    var bb = bit + 64 * brr.Length;
+                    if (bb >= b.Length) break;
+                    l.Add(bb);
+                }
             return l.AsSpan().ToArray();
+        }
+
+        /// <summary>
+        /// フラグの立っているインデックスの数を数えます。
+        /// </summary>
+        [凾(256)]
+        public static int PopCount(this BitArray b)
+        {
+            var arr = b.GetArray().AsSpan();
+            var rem = b.Length & 31;
+            int cnt = rem == 0 ? 0 : -BitOperations.PopCount((uint)((-1 << rem) & arr[^1]));
+            if ((arr.Length & 1) != 0)
+            {
+                cnt += BitOperations.PopCount((uint)arr[^1]);
+                arr = arr[..^1];
+            }
+            var brr = MemoryMarshal.Cast<int, ulong>(arr);
+            foreach (var bb in brr)
+                cnt += BitOperations.PopCount(bb);
+            return cnt;
+        }
+
+        /// <summary>
+        /// フラグの立っている最下位インデックス。すべて false のときは未定義
+        /// </summary>
+        [凾(256)]
+        public static int LeadingZeroCount(this BitArray b)
+        {
+            var arr = b.GetArray().AsSpan();
+            var brr = MemoryMarshal.Cast<int, ulong>(arr);
+            for (int i = 0; i < brr.Length; i++)
+                if (brr[i] != 0)
+                    return Math.Min(b.Length, BitOperations.LeadingZeroCount(brr[i]) + i * 64);
+            if ((arr.Length & 1) != 0)
+                return Math.Min(b.Length, BitOperations.LeadingZeroCount((uint)arr[^1]) + brr.Length * 64);
+            return b.Length;
         }
     }
 }
