ada44082a833c1974c77d0ce1f4a4b63bb9a55bf
kzrnm <gengesa@gmail.com>
2023-01-02 22:04:35 +0900

    Test.cs → Tests.cs

---
+++ Library/Competitive.Library.Test/Collection/PersistentSetTests.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kzrnm.Competitive.Testing.Collection
+{
+    public class PersistentSetTests
+    {
+        [Fact]
+        public void Set()
+        {
+        }
+    }
+}
