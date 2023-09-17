41f590245070430bd6421b3e7a39b5a2a528ea3f
kzrnm <gengesa@gmail.com>
2023-09-18 02:37:17 +0900

    BigInteger の掛け算のlogを改善

--- Library/Checker/Number/BigIntegerDecimalTests.cs
+++ Library/Checker/Number/BigIntegerDecimalTests.cs
@@ -3,12 +3,20 @@ using Kzrnm.Competitive.IO;
 
 namespace Kzrnm.Competitive.Number
 {
-    internal class BigIntegerAdditionTest : BaseSolver
+    internal class BigIntegerDecimalAdditionTest : BaseSolver
     {
         public override string Url => "https://judge.yosupo.jp/problem/addition_of_big_integers";
         public override ConsoleOutput? Solve(ConsoleReader cr, Utf8ConsoleWriter cw)
         {
             return (BigIntegerDecimal.Parse(cr.AsciiChars()) + BigIntegerDecimal.Parse(cr.AsciiChars())).ToString();
+        }
+    }
+    internal class BigIntegerDecimalMultiplicationTest : BaseSolver
+    {
+        public override string Url => "https://judge.yosupo.jp/problem/multiplication_of_big_integers";
+        public override ConsoleOutput? Solve(ConsoleReader cr, Utf8ConsoleWriter cw)
+        {
+            return (BigIntegerDecimal.Parse(cr.AsciiChars()) * BigIntegerDecimal.Parse(cr.AsciiChars())).ToString();
         }
     }
     //internal class BigIntegerDivisionTest : BaseSolver
