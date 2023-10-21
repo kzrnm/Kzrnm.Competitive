7388a94bf4b21b395db00138f2300e40627f45a7
kzrnm <gengesa@gmail.com>
2023-10-22 01:51:38 +0900

    Expand Test failure

---
+++ Library/Expand.Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SourceExpander.Testing;
+partial class Program
+{
+    const bool __ManyTestCases = false;
+    static void Main() => new Program(new(), new()).Run();
+    private SourceExpander.Testing.Kzrnm.Competitive.ConsoleOutput? Calc()
+    {
+        return 1;
+    }
+}
