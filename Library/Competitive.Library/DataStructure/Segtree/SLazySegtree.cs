c21041148f40e8043ee385c7e9ba2f427c05c333
kzrnm <gengesa@gmail.com>
2023-10-30 02:10:10 +0900

    セグ木を整理

--- Library/Competitive.Library/DataStructure/Segtree/SLazySegtree.cs
+++ Library/Competitive.Library/DataStructure/Segtree/SLazySegtree.cs
@@ -1,4 +1,3 @@
-using AtCoder;
 using AtCoder.Internal;
 using System;
 using System.Collections.Generic;
@@ -7,36 +6,6 @@ using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
 
 namespace Kzrnm.Competitive
 {
-    /// <summary>
-    /// モノイドを定義するインターフェイスです。
-    /// </summary>
-    /// <typeparam name="T">操作を行う型。</typeparam>
-    /// <typeparam name="F">写像の型。</typeparam>
-    [IsOperator]
-    public interface ISLazySegtreeOperator<T, F>
-    {
-        /// <summary>
-        /// <c>Operate(x, Identity) = x</c> を満たす単位元。
-        /// </summary>
-        T Identity { get; }
-        /// <summary>
-        /// <c>Mapping(FIdentity, x) = x</c> を満たす恒等写像。
-        /// </summary>
-        F FIdentity { get; }
-        /// <summary>
-        /// 結合律 Operate(Operate(a, b), c) = Operate(a, Operate(b, c)) を満たす写像。
-        /// </summary>
-        T Operate(T x, T y);
-        /// <summary>
-        /// 写像　<paramref name="f"/> を 大きさ <paramref name="size"/> の <paramref name="x"/> に作用させる関数。
-        /// </summary>
-        T Mapping(F f, T x, int size);
-        /// <summary>
-        /// 写像　<paramref name="nf"/> を既存の写像 <paramref name="cf"/> に対して合成した写像 <paramref name="nf"/>∘<paramref name="cf"/>。
-        /// </summary>
-        F Composition(F nf, F cf);
-    }
-
     /// <summary>
     /// 長さ N の配列に対し、
     /// <list type="bullet">
---
+++ Library/Competitive.Library/DataStructure/Segtree/SLazySegtree.md
@@ -0,0 +1,9 @@
+---
+title: 遅延セグ木
+documentation_of: ./SLazySegtree.cs
+---
+
+
+## 概要
+
+ac-library-csharp と異なる点として、`Mapping(F, T)` ではなく `Mapping(F, T)` を持っている。区間加算・区間和のようにノードのサイズが必要なデータを保持するときに有用。
