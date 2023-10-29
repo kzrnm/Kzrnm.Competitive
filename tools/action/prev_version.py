36634a5293a5f1d2d3406b0134eca3183542cf7e
kzrnm <gengesa@gmail.com>
2023-10-30 00:52:58 +0900

    月一回の更新にする

---
+++ tools/action/prev_version.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import json
+from urllib.request import urlopen
+from typing import NamedTuple
+
+
+class Version(NamedTuple):
+    major: int
+    minor: int
+    patch: int
+
+    @classmethod
+    def parse(cls, s: str) -> "Version":
+        splited = dict(enumerate(int(p.strip()) for p in s.split(".")))
+        return Version(splited.get(0), splited.get(1), splited.get(2))
+
+    def date_num(self) -> int:
+        return self.major * 100 + self.minor // 100
+
+
+def main(prev: Version):
+    with urlopen(
+        "https://api.nuget.org/v3-flatcontainer/kzrnm.competitive/index.json"
+    ) as req:
+        latest = Version.parse(json.load(req)["versions"][-1])
+    print(prev.date_num(), file=sys.stderr)
+    print(latest.date_num(), file=sys.stderr)
+    print(f"is-new-month={prev.date_num()<latest.date_num()}".lower())
+
+
+if __name__ == "__main__":
+    main(Version.parse(sys.argv[1]))
--- tools/cv-docs.sh
+++ tools/cv-docs.sh
@@ -4,4 +4,4 @@ ROOT=$(dirname $(dirname $(readlink -f "$0")))
 cd "$ROOT/Library/"
 
 competitive-verifier docs .competitive-verifier/tmp/Result-*/result.json --verify-json .competitive-verifier/tmp/verify_files.json --destination .competitive-verifier/tmp/_jekyll/
-python3 "$ROOT/tools/update_redirect.py" "$ROOT/Library/.competitive-verifier/tmp/_jekyll/"
+python3 "$ROOT/tools/action/update_redirect.py" "$ROOT/Library/.competitive-verifier/tmp/_jekyll/"
