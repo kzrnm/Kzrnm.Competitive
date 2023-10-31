0f1f443897b9b78e8417af3608f70e4c2045a244
kzrnm <gengesa@gmail.com>
2023-11-01 03:55:06 +0900

    Update cond

--- tools/action/prev_version.py
+++ tools/action/prev_version.py
@@ -19,13 +19,13 @@ class Version(NamedTuple):
         return self.major * 100 + self.minor // 100
 
 
-def main(prev: Version):
+def main(latest: Version):
     with urlopen(
         "https://api.nuget.org/v3-flatcontainer/kzrnm.competitive/index.json"
     ) as req:
-        latest = Version.parse(json.load(req)["versions"][-1])
-    print(f"prev={prev.date_num()}", file=sys.stderr)
-    print(f"latest={latest.date_num()}", file=sys.stderr)
+        prev = Version.parse(json.load(req)["versions"][-1])
+    print(f"prev={prev.date_num()}")
+    print(f"latest={latest.date_num()}")
     print(f"is-new-month={prev.date_num()<latest.date_num()}".lower())
 
 
