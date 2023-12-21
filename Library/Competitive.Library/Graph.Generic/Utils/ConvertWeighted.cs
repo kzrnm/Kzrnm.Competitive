58dba43e67a97539a283960879d6c056bf1be793
kzrnm <gengesa@gmail.com>
2023-12-22 01:16:07 +0900

    Add TITLE

---
+++ Library/Competitive.Library/Graph.Generic/Utils/ConvertWeighted.cs
@@ -0,0 +1,37 @@
+using AtCoder.Internal;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
+
+namespace Kzrnm.Competitive
+{
+    public static class ___GraphWeighted
+    {
+        public static IGraph<TEdge> AsUnweighted<T, TEdge>(this IWGraph<T, TEdge> g)
+            where TEdge : IWGraphEdge<T>
+            => g;
+
+        public static ITreeGraph<TNode, TEdge> AsUnweighted<T, TNode, TEdge>(this IWTreeGraph<T, TNode, TEdge> g)
+            where TNode : ITreeNode<TEdge>
+            where TEdge : IWGraphEdge<T>
+            => g;
+
+        [DebuggerDisplay("{" + nameof(To) + "}")]
+        public readonly record struct OneEdge(int To) : IGraphEdge<OneEdge>, IWGraphEdge<uint>
+        {
+            public static OneEdge None => new(-1);
+            [凾(256)] public static implicit operator int(OneEdge e) => e.To;
+            [凾(256)] public OneEdge Reversed(int from) => new(from);
+            uint IWGraphEdge<uint>.Value => 1;
+        }
+        private class Weighted : SimpleGraph<OneEdge>, IWGraph<uint, OneEdge>
+        {
+            public Weighted(GraphNode<OneEdge>[] array, Csr<OneEdge> edges) : base(array, edges)
+            {
+            }
+        }
+        public static IWGraph<uint, OneEdge> ToWeighted(this IGraph<GraphEdge> graph)
+            => new Weighted(Unsafe.As<GraphNode<OneEdge>[]>(graph.AsArray()), Unsafe.As<Csr<OneEdge>>(graph.Edges));
+    }
+}
--- Library/Competitive.Library/Graph.Generic/WGraph.cs
+++ Library/Competitive.Library/Graph.Generic/WGraph.cs
@@ -5,6 +5,29 @@ using 凾 = System.Runtime.CompilerServices.MethodImplAttribute;
 
 namespace Kzrnm.Competitive
 {
+    public interface IWGraph<T, TEdge> : IGraph<TEdge>
+        where TEdge : IWGraphEdge<T>
+    { }
+    public interface IWTreeGraph<T, TNode, TEdge> : ITreeGraph<TNode, TEdge>
+        where TNode : ITreeNode<TEdge>
+        where TEdge : IWGraphEdge<T>
+    { }
+
+    public interface IWGraphEdge<T> : IGraphEdge
+    {
+        /// <summary>
+        /// 重み
+        /// </summary>
+        T Value { get; }
+    }
+    public interface IWTreeNode<T>
+    {
+        /// <summary>
+        /// 根からの重みの総和
+        /// </summary>
+        public T DepthLength { get; }
+    }
+    
     public class WGraph<T, TEdge> : IWGraph<T, TEdge>, IGraph<WGraph<T, TEdge>, TEdge>
         where TEdge : IWGraphEdge<T>
     {
