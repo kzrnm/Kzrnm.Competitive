237ac8ef494039cbee51b37b05da671073dca15f
kzrnm <gengesa@gmail.com>
2025-01-22 04:55:17 +0900

    .NET9 LINQ

--- Library/Competitive.Library/Extensions/Linq/MyLinqExtension.cs
+++ Library/Competitive.Library/Extensions/Linq/MyLinqExtension.cs
@@ -7,24 +7,30 @@ namespace Kzrnm.Competitive
 {
     public static class __CollectionExtension_MyLinq
     {
+#if !NET9_0_OR_GREATER
         /// <summary>
         /// インデックスをつける
         /// </summary>
         [凾(256)]
-        public static IEnumerable<(TSource Value, int Index)> Indexed<TSource>(this IEnumerable<TSource> source)
-              => source.Select((v, i) => (v, i));
-
+        public static IEnumerable<(int Index, TSource Item)> Index<TSource>(this IEnumerable<TSource> source)
+              => source.Select((v, i) => (i, v));
+#endif
         /// <summary>
         /// 条件に合致するインデックスを返す
         /// </summary>
         [凾(256)]
         public static IEnumerable<int> WhereBy<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
-              => source.Indexed().Where(t => predicate(t.Value)).Select(t => t.Index);
+                  => source.Index().Where(t => predicate(t.Item)).Select(t => t.Index);
         /// <summary>
         /// 要素の個数を返します。
         /// </summary>
         [凾(256)]
-        public static Dictionary<TKey, int> GroupCount<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) => source.GroupBy(keySelector).ToDictionary(g => g.Key, g => g.Count());
+        public static Dictionary<TKey, int> GroupCount<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+#if NET9_0_OR_GREATER
+            => source.CountBy(keySelector).ToDictionary();
+#else
+            => source.GroupBy(keySelector).ToDictionary(g => g.Key, g => g.Count());
+#endif
         /// <summary>
         /// 要素の個数を返します。
         /// </summary>
@@ -36,12 +42,12 @@ namespace Kzrnm.Competitive
         /// </summary>
         [凾(256)]
         public static IEnumerable<IGrouping<TKey, int>> GroupIndex<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-            => source.Indexed().GroupBy(t => keySelector(t.Value), t => t.Index);
+            => source.Index().GroupBy(t => keySelector(t.Item), t => t.Index);
         /// <summary>
         /// 要素をグループ化し、インデックスを保持します。
         /// </summary>
         [凾(256)]
         public static IEnumerable<IGrouping<TKey, int>> GroupIndex<TKey>(this IEnumerable<TKey> source)
-            => source.Indexed().GroupBy(t => t.Value, t => t.Index);
+            => source.Index().GroupBy(t => t.Item, t => t.Index);
     }
 }
