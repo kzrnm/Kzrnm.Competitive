6404d0cd37cc69867c87f285a8cea9a44ef8822b
kzrnm <gengesa@gmail.com>
2025-04-19 00:02:05 +0900

    Update .sh

--- Library/.competitive-verifier/download.sh
+++ Library/.competitive-verifier/download.sh
@@ -5,4 +5,4 @@ id=$(gh run list -R kzrnm/Kzrnm.Competitive -L 1 -w verify --json databaseId --j
 echo "Wokflow ID=$id"
 dst="$(dirname $0)/tmp"
 rm -r "$dst"
-gh run download $id -R kzrnm/Kzrnm.Competitive -D "$dst"
+gh run download "$id" -R kzrnm/Kzrnm.Competitive -D "$dst"
